6cfff2aa922e3a8278c7426b1956b3bd9ce6252b
Martin Pool <mbp@sourcefrog.net>
2020-11-30 21:27:22 -0800

    Solve dec01b

--- src/bin/dec01.rs
+++ src/bin/dec01.rs
@@ -1,5 +1,8 @@
+use std::collections::HashSet;
+
 pub fn main() {
-    println!("dec01 a: {}", solve_a());
+    println!("dec01a: {}", solve_a());
+    println!("dec01b: {}", solve_b());
 }
 
 fn solve_a() -> isize {
@@ -15,10 +18,38 @@ fn solve_a() -> isize {
     panic!("no combination found");
 }
 
+fn solve_b() -> isize {
+    let vals: HashSet<isize> = load_input().into_iter().collect();
+    for a in &vals {
+        for b in &vals {
+            let c = 2020 - a - b;
+            if c > 0 && vals.contains(&c) {
+                return a * b * c;
+            }
+        }
+    }
+    panic!("no combination found");
+}
+
 fn load_input() -> Vec<isize> {
     std::fs::read_to_string("input/dec01.txt")
         .unwrap()
         .lines()
         .map(|l| l.parse().unwrap())
         .collect()
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn solution_a() {
+        assert_eq!(solve_a(), 646779);
+    }
+
+    #[test]
+    fn solution_b() {
+        assert_eq!(solve_b(), 246191688);
+    }
 }
