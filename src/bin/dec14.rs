ea003e2ee55b82c137504b5116275198ae3c8509
Martin Pool <mbp@sourcefrog.net>
2020-12-13 22:08:00 -0800

    Rewrite to use Nom rather than ad-hoc parsing
    Slower to write, but probably more robust

--- src/bin/dec14.rs
+++ src/bin/dec14.rs
@@ -14,36 +14,80 @@ use std::collections::HashMap;
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+use nom::branch::*;
+use nom::bytes::complete::*;
+use nom::character::complete::*;
+use nom::combinator::*;
+use nom::multi::*;
+use nom::sequence::*;
+use nom::{IResult, Parser};
+
 pub fn main() {
     println!("14a: {}", solve_a());
     println!("14b: {}", solve_b());
+}
+
+enum Instruction {
+    Mask(String),
+    Store(u64, u64),
+}
+use Instruction::*;
+
+fn parse_input(s: &str) -> IResult<&str, Vec<Instruction>> {
+    all_consuming(many1(terminated(
+        alt((parse_mask, parse_store)),
+        multispace1,
+    )))(s)
+}
+
+fn parse_mask(s: &str) -> IResult<&str, Instruction> {
+    preceded(
+        tag("mask = "),
+        recognize(many0(one_of("01X"))).map(|s: &str| Instruction::Mask(s.to_owned())),
+    )(s)
+}
+
+fn parse_store(s: &str) -> IResult<&str, Instruction> {
+    map(
+        pair(
+            delimited(
+                tag("mem["),
+                map_res(digit1, |a: &str| a.parse()),
+                tag("] = "),
+            ),
+            map_res(digit1, |v: &str| v.parse()),
+        ),
+        |(a, v)| Instruction::Store(a, v),
+    )(s)
 }
 
 fn solve_a() -> u64 {
     let mut maskbits: u64 = 0;
     let mut setbits: u64 = 0;
     let mut mem = vec![0u64; 100000];
-    for l in std::fs::read_to_string("input/dec14.txt").unwrap().lines() {
-        if let Some(mask) = l.strip_prefix("mask = ") {
-            // dbg!(&mask);
-            maskbits = 0;
-            setbits = 0;
-            for (i, c) in mask.chars().enumerate() {
-                let b = 1 << (mask.len() - i - 1);
-                if c != 'X' {
-                    maskbits |= b;
-                }
-                if c == '1' {
-                    setbits |= b;
+    for inst in parse_input(&std::fs::read_to_string("input/dec14.txt").unwrap())
+        .unwrap()
+        .1
+        .into_iter()
+    {
+        match inst {
+            Mask(mask) => {
+                // dbg!(&mask);
+                maskbits = 0;
+                setbits = 0;
+                for (i, c) in mask.chars().enumerate() {
+                    let b = 1 << (mask.len() - i - 1);
+                    if c != 'X' {
+                        maskbits |= b;
+                    }
+                    if c == '1' {
+                        setbits |= b;
+                    }
                 }
             }
-        } else {
-            let l = l.strip_prefix("mem[").unwrap();
-            let mut parts = l.split("] = ");
-            let addr: usize = parts.next().unwrap().parse().unwrap();
-            let val: u64 = parts.next().unwrap().parse().unwrap();
-            // dbg!(&addr, &val);
-            mem[addr] = (val & !maskbits) | setbits;
+            Store(addr, val) => {
+                mem[addr as usize] = (val & !maskbits) | setbits;
+            }
         }
     }
     mem.iter().sum()
@@ -71,14 +115,13 @@ fn solve_b() -> u64 {
             let mut addr: u64 = parts.next().unwrap().parse().unwrap();
             let val: u64 = parts.next().unwrap().parse().unwrap();
             addr |= setbits;
-            let combos = 1 << floatbits.len();
-            for i in 0..combos {
+            for i in 0..(1 << floatbits.len()) {
                 let mut thisaddr = addr;
                 for (j, float) in floatbits.iter().enumerate() {
                     if i & (1 << j) != 0 {
-                        thisaddr |= (1 << float)
+                        thisaddr |= float;
                     } else {
-                        thisaddr &= !(1 << float)
+                        thisaddr &= !float;
                     }
                 }
                 mem.insert(thisaddr, val);
