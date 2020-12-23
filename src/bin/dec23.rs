d50d31f1f3ab84969091d60b85f8664a621ad697
Martin Pool <mbp@sourcefrog.net>
2020-12-22 22:54:09 -0800

    Refactor to linked representation

--- src/bin/dec23.rs
+++ src/bin/dec23.rs
@@ -16,59 +16,68 @@
 
 #![allow(unused_imports, dead_code, unused_mut)]
 
-use std::collections::BTreeSet;
+// use std::collections::BTreeSet;
 
 const INPUT: &'static str = "562893147";
 // const INPUT: &'static str = "389125467";
 
 pub fn main() {
-    println!("23a: {}", Ring::from_str(INPUT).play_a());
-    // println!("23b: {}", Ring::from_str(INPUT).solve_b());
+    // println!("23a: {}", Ring::from_str(INPUT).solve_a());
+    println!("23b: {}", Ring::solve_b());
 }
 
 struct Ring {
-    v: Vec<usize>,
     n: usize,
+
+    /// succ[i] is the number that comes after i in the ring,
+    /// where i>=1
+    succ: Vec<usize>,
+
+    /// Number of the first card.
+    first: usize,
 }
 
 impl Ring {
     fn new(v: Vec<usize>) -> Ring {
-        Ring { n: v.len(), v }
-    }
-
-    fn remove_3(&mut self, pos: usize) -> Vec<usize> {
-        let mut r = Vec::new();
-        let mut rmpos = pos + 1;
-        for i in 0..3 {
-            if rmpos < self.v.len() {
-                r.push(self.v.remove(rmpos));
-            } else {
-                r.push(self.v.remove(0));
-            }
+        let n = v.len();
+        assert_eq!(*v.iter().max().unwrap(), n);
+        let mut succ = vec![0; n + 1];
+        for i in 0..n - 1 {
+            succ[v[i]] = v[i + 1];
         }
+        succ[v[n - 1]] = v[0];
+        // dbg!(&succ);
+        let r = Ring {
+            n: v.len(),
+            first: v[0],
+            succ,
+        };
+        r.check();
         r
     }
 
-    fn insert_after(&mut self, pos: usize, vals: Vec<usize>) {
-        if pos + 1 >= self.v.len() {
-            self.v.extend_from_slice(&vals)
-        } else {
-            for (i, v) in vals.iter().enumerate() {
-                self.v.insert(pos + 1 + i, *v)
-            }
+    /// Remove and return the values of the 3 cup that come after the cup
+    /// labeled c.
+    fn unlink_3_after(&mut self, c: usize) -> Vec<usize> {
+        let mut r = Vec::new();
+        let mut prev = c;
+        for _i in 0..3 {
+            let next = self.succ[prev];
+            r.push(next);
+            prev = next;
         }
+        self.succ[c] = self.succ[prev];
+        r
     }
 
-    fn from_str(s: &str) -> Ring {
-        Ring::new(
-            s.chars()
-                .map(|c| c.to_digit(10).unwrap() as usize)
-                .collect(),
-        )
-    }
-
-    fn position(&self, a: usize) -> Option<usize> {
-        self.v.iter().position(|x| *x == a)
+    fn insert_after(&mut self, c: usize, vals: Vec<usize>) {
+        let mut prev = c;
+        let follows = self.succ[c];
+        for v in vals {
+            self.succ[prev] = v;
+            prev = v;
+        }
+        self.succ[prev] = follows;
     }
 
     fn wrap_sub(&self, a: usize, b: usize) -> usize {
@@ -86,70 +95,103 @@ impl Ring {
     //     r
     // }
 
-    fn play_a(&mut self) -> String {
-        let mut current = 0;
-        for mv in 1..=100 {
-            println!("move {}", mv);
-            let current_val = self.v[current];
-            println!(
-                "cups: {:?}, current value {} in position {}",
-                self.v, current_val, current,
-            );
-            let taken = self.remove_3(current);
-            println!("taken {:?}", taken);
-            let mut dest_pos: Option<usize> = None;
-            for i in 1..self.n {
-                let dest_val = self.wrap_sub(current_val, i);
-                dbg!(dest_val);
-                dest_pos = self.position(dest_val);
-                if dest_pos.is_some() {
-                    assert_eq!(self.v[dest_pos.unwrap()], dest_val);
-                    println!(
-                        "dest val {} is position {} in {:?}",
-                        dest_val,
-                        dest_pos.unwrap(),
-                        self.v
-                    );
+    fn solve_a(&mut self) -> String {
+        self.play(100);
+
+        self.a_result()
+    }
+
+    fn to_value_vec(&self) -> Vec<usize> {
+        let mut r = Vec::new();
+        let mut a = 1;
+        loop {
+            r.push(a);
+            a = self.succ[a];
+            if a == 1 {
+                return r;
+            }
+        }
+    }
+
+    fn play(&mut self, rounds: usize) {
+        let mut current = self.first;
+        self.check();
+
+        for round in 1..=rounds {
+            assert!(current > 0 && current <= self.n);
+            if round % 1000 == 0 {
+                println!("round {}", round);
+            }
+            // println!("deck: {:?}", self.to_value_vec());
+            // dbg!(round, current, &self.succ);
+            // println!(
+            //     "cups: {:?}, current value {} in position {}",
+            //     self.v, current_val, current,
+            // );
+            let taken = self.unlink_3_after(current);
+            // println!("taken {:?}", taken);
+            let mut dest = current;
+            loop {
+                dest = self.wrap_sub(dest, 1);
+                if !contains(&taken, dest) {
                     break;
                 }
             }
-            self.insert_after(dest_pos.unwrap(), taken);
-            debug_assert_eq!(self.v.len(), self.n);
+            assert!(dest > 0 && dest <= self.n);
+            // println!("insert after {}: {:?}", dest, taken);
+            self.insert_after(dest, taken);
             self.check();
-            current = (self.position(current_val).unwrap() + 1) % self.n;
-            println!();
+            current = self.succ[current];
         }
-        println!("final: {:?}", self.v);
-        self.a_result()
-        // 78459236 is wrong
+        // println!("final: {:?}", self.v);
     }
 
     fn a_result(&self) -> String {
         let mut r = String::new();
-        let one_pos = self.v.iter().position(|x| *x == 1).unwrap();
-        for i in 1..self.n {
-            r.push(std::char::from_digit(self.v[(one_pos + i) % self.n] as u32, 10).unwrap())
+        let mut a = self.succ[1];
+        dbg!(&self.succ);
+        while a != 1 {
+            r.push(std::char::from_digit(a as u32, 10).unwrap());
+            a = self.succ[a];
         }
         r
     }
 
+    #[cfg(not(debug_assertions))]
+    fn check(&self) {}
+
+    #[cfg(debug_assertions)]
     fn check(&self) {
+        let mut seen = vec![false; self.n + 1];
+        debug_assert_eq!(self.n + 1, self.succ.len());
+        assert_eq!(self.succ[0], 0);
+        assert_eq!(*self.succ[1..].iter().min().unwrap(), 1);
+        assert_eq!(*self.succ[1..].iter().max().unwrap(), self.n);
         for i in 1..=self.n {
-            debug_assert!(
-                self.v.iter().find(|x| **x == i).is_some(),
-                "couldn't find {} in {:?}",
-                i,
-                self.v
-            );
+            assert_ne!(self.succ[i], i);
+            assert_eq!(seen[i], false);
+            seen[i] = true;
         }
+        assert!(seen.iter().skip(1).all(|x| *x));
     }
 
-    fn solve_b(&mut self) -> usize {
-        self.v.extend(self.n..=1_000_000);
-        assert_eq!(self.v.len(), 1_000_000);
-        self.n = self.v.len();
-        0
+    fn solve_b() -> usize {
+        let mut v = parse(INPUT);
+        v.extend(v.len() + 1..=1_000_000);
+        assert_eq!(v.len(), 1_000_000);
+        Ring::new(v).play(10_000_000);
+        todo!()
     }
+}
+
+fn contains(a: &[usize], b: usize) -> bool {
+    a.iter().position(|x| *x == b).is_some()
+}
+
+fn parse(s: &str) -> Vec<usize> {
+    s.chars()
+        .map(|c| c.to_digit(10).unwrap() as usize)
+        .collect()
 }
 
 #[cfg(test)]
@@ -158,7 +200,7 @@ mod test {
 
     #[test]
     fn solution_a() {
-        assert_eq!(Ring::from_str(INPUT).play_a(), "38925764");
+        assert_eq!(Ring::new(parse(INPUT)).solve_a(), "38925764");
     }
 
     #[test]
