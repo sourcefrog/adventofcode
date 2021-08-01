b5d0fecc088baa32cd634f83d580b6ed4a28b692
Martin Pool <mbp@sourcefrog.net>
2021-08-01 16:06:53 -0700

    Start on 2016

---
+++ 2016/src/bin/1601.rs
@@ -0,0 +1,21 @@
+enum Dir {
+    Left, 
+    Right,
+}
+
+struct Step {
+    dir: Dir,
+    len: usize,
+}
+
+fn load() -> Vec<Step> {
+    todo!()
+}
+
+fn solve_a() -> usize {
+    0
+}
+
+pub fn main() {
+    println!("{}", solve_a());
+}
