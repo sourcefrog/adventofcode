6702a25245702460da5459db6a36bb0e4c5900cf
Martin Pool <mbp@sourcefrog.net>
2021-08-21 13:22:21 -0700

    Solve 1603b

--- 2016/src/bin/1603.rs
+++ 2016/src/bin/1603.rs
@@ -8,14 +8,31 @@ fn solve_type_a(s: &str) -> usize {
                 .split_ascii_whitespace()
                 .map(|w| w.parse().expect("parse integer length"))
                 .collect();
-            debug_assert_eq!(s.len(), 3);
-            (s[0] + s[1] > s[2]) && (s[1] + s[2] > s[0]) && (s[0] + s[2] > s[1])
+            is_possible_triangle(&s)
         })
         .count()
 }
 
-fn solve_type_b(s: &str) -> String {
-    todo!()
+fn is_possible_triangle(s: &[usize]) -> bool {
+    debug_assert_eq!(s.len(), 3);
+    (s[0] + s[1] > s[2]) && (s[1] + s[2] > s[0]) && (s[0] + s[2] > s[1])
+}
+
+fn solve_type_b(input: &str) -> usize {
+    let s: Vec<Vec<usize>> = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|word| word.parse().expect("parse integer length"))
+                .collect()
+        })
+        .collect();
+    s.as_slice()
+        .chunks(3)
+        .flat_map(|ch| {
+            (0..3).filter(move |&col| is_possible_triangle(&[ch[0][col], ch[1][col], ch[2][col]]))
+        })
+        .count()
 }
 
 fn input() -> String {
@@ -26,36 +43,26 @@ fn solve_a() -> usize {
     solve_type_a(&input())
 }
 
-fn solve_b() -> String {
+fn solve_b() -> usize {
     solve_type_b(&input())
 }
 
 fn main() {
     println!("1602a: {}", solve_a());
-    // println!("1602b: {}", solve_b());
+    println!("1602b: {}", solve_b());
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
 
-    // #[test]
-    // fn example_a() {
-    //     assert_eq!(solve_type_a(EXAMPLE), "1985");
-    // }
-
-    // #[test]
-    // fn example_b() {
-    //     assert_eq!(solve_type_b(EXAMPLE), "5DB3");
-    // }
-
     #[test]
     fn solution_a() {
         assert_eq!(solve_a(), 1050);
     }
 
-    // #[test]
-    // fn solution_b() {
-    //     assert_eq!(solve_b(), "7423A");
-    // }
+    #[test]
+    fn solution_b() {
+        assert_eq!(solve_b(), 1921);
+    }
 }
