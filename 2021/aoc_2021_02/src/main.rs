77c10d947e3d3f533b77dad6b0295f559e80aa2b
Martin Pool <mbp@sourcefrog.net>
2021-12-01 21:08:26 -0800

    Solve 2021_02

---
+++ 2021/aoc_2021_02/src/main.rs
@@ -0,0 +1,61 @@
+//! https://adventofcode.com/2021/day/2
+
+fn main() {
+    println!("{}", solve_a(&input()));
+    println!("{}", solve_b(&input()));
+}
+
+fn input() -> String {
+    std::fs::read_to_string("input.txt").unwrap()
+}
+
+fn solve_a(input: &str) -> usize {
+    let mut x = 0;
+    let mut y = 0;
+    for l in input.lines() {
+        let (cmd, n) = l.split_once(' ').unwrap();
+        let n: usize = n.parse().unwrap();
+        match cmd {
+            "forward" => x += n,
+            "down" => y += n,
+            "up" => y -= n,
+            _ => panic!(),
+        }
+    }
+    x * y
+}
+
+fn solve_b(input: &str) -> usize {
+    let mut x = 0;
+    let mut y = 0;
+    let mut aim = 0;
+    for l in input.lines() {
+        let (cmd, n) = l.split_once(' ').unwrap();
+        let n: usize = n.parse().unwrap();
+        match cmd {
+            "forward" => {
+                x += n;
+                y += aim * n;
+            }
+            "down" => aim += n,
+            "up" => aim -= n,
+            _ => panic!(),
+        }
+    }
+    x * y
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn solution_a() {
+        assert_eq!(solve_a(&input()), 1524750);
+    }
+
+    #[test]
+    fn solution_b() {
+        assert_eq!(solve_b(&input()), 1592426537);
+    }
+}
