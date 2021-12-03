4a0d79c3dda642a573f4cae17cddb2c389de09eb
Martin Pool <mbp@sourcefrog.net>
2021-12-03 11:45:55 -0800

    Remove obsolete FromRows

--- aoclib/src/matrix.rs
+++ aoclib/src/matrix.rs
@@ -36,14 +36,6 @@ impl<T: Clone> Matrix<T> {
         }
     }
 
-    /// Make a builder that will accumulate rows of a matrix.
-    pub fn from_rows() -> FromRows<T> {
-        FromRows::<T> {
-            w: 0,
-            d: Vec::new(),
-        }
-    }
-
     pub fn width(&self) -> usize {
         self.w as usize
     }
@@ -175,34 +167,6 @@ impl<T: Clone> IndexMut<Point> for Matrix<T> {
     }
 }
 
-pub struct FromRows<T> {
-    w: usize,
-    d: Vec<T>,
-}
-
-impl<T: Clone> FromRows<T> {
-    pub fn add_row(&mut self, r: &[T]) {
-        if self.d.is_empty() {
-            // First row
-            assert!(!r.is_empty());
-            self.w = r.len();
-        } else {
-            assert_eq!(r.len(), self.w, "Rows must be the same length");
-        }
-        self.d.extend_from_slice(r);
-    }
-
-    pub fn finish(mut self) -> Matrix<T> {
-        self.d.shrink_to_fit();
-        assert!(self.d.len() % self.w == 0, "Matrix isn't rectangular");
-        Matrix {
-            w: self.w as isize,
-            h: (self.d.len() / self.w) as isize,
-            d: self.d,
-        }
-    }
-}
-
 /// Build a Matrix from an iterator of rows, each item of which is an iterator of cells.
 ///
 /// All the rows must be the same length.
@@ -245,20 +209,6 @@ mod test {
         m[point(6, 6)] = 10;
         assert_eq!(m[point(6, 6)], 10);
         assert_eq!(m[point(5, 5)], 7u8);
-    }
-
-    #[test]
-    fn from_rows() {
-        let mut b = Matrix::from_rows();
-        b.add_row(&[1, 2, 3]);
-        b.add_row(&[4, 5, 6]);
-        b.add_row(&[7, 8, 9]);
-        let m = b.finish();
-        assert_eq!(m.width(), 3);
-        assert_eq!(m.height(), 3);
-        assert_eq!(m[point(0, 0)], 1);
-        assert_eq!(m[point(2, 0)], 3);
-        assert_eq!(m[point(2, 2)], 9);
     }
 
     #[test]
