d17e227ff6d090677b472dd8cb9eb62257fcbfb1
Martin Pool <mbp@sourcefrog.net>
2021-12-03 08:10:32 -0800

    Refactor to use common Matrix

--- 2021/aoc_2021_03/src/main.rs
+++ 2021/aoc_2021_03/src/main.rs
@@ -1,5 +1,7 @@
 //! https://adventofcode.com/2021/day/3
 
+use aoclib::{point, Matrix};
+
 fn main() {
     println!("{}", solve_a(&input()));
     println!("{}", solve_b(&input()));
@@ -9,23 +11,23 @@ fn input() -> String {
     std::fs::read_to_string("input.txt").unwrap()
 }
 
-fn to_matrix(input: &str) -> Vec<Vec<bool>> {
+fn to_matrix(input: &str) -> Matrix<bool> {
+    let mut from_rows = Matrix::from_rows();
     input
         .lines()
-        .map(|l| l.chars().map(|c| c == '1').collect())
-        .collect()
+        .map(|l| l.chars().map(|c| c == '1').collect::<Vec<bool>>())
+        .for_each(|r| from_rows.add_row(&r));
+    from_rows.finish()
 }
 
 fn solve_a(input: &str) -> usize {
     let m = to_matrix(input);
-    let n = m.len();
-    let cols = m[0].len();
+    let n = m.height();
+    let cols = m.width();
     let mut ones = vec![0; cols];
-    for l in m {
-        for (i, c) in l.iter().enumerate() {
-            if *c {
-                ones[i] += 1
-            }
+    for point in m.iter_points() {
+        if m[point] {
+            ones[point.x as usize] += 1;
         }
     }
     let mut gamma = 0;
@@ -42,46 +44,51 @@ fn solve_a(input: &str) -> usize {
     gamma * epsilon
 }
 
-fn count(a: &[&[bool]], i: usize) -> (usize, usize) {
-    let ones = a.iter().filter(|l| l[i]).count();
-    (ones, a.len() - ones)
+fn count(m: &Matrix<bool>, rows: &[usize], col: usize) -> (usize, usize) {
+    let ones = rows
+        .iter()
+        .filter(|row| m[point(col as isize, **row as isize)])
+        .count();
+    (ones, rows.len() - ones)
 }
 
-fn from_base2(a: &[bool]) -> usize {
+fn from_base2(matrix: &Matrix<bool>, row: usize) -> usize {
     let mut x = 0;
-    for c in a {
-        x = (x << 1) | (*c as usize);
+    for col in 0..matrix.width() {
+        let c = matrix[point(col as isize, row as isize)];
+        x = (x << 1) | (c as usize);
     }
     x
 }
 
 fn solve_b(input: &str) -> usize {
     let m = to_matrix(input);
-    let n = m.len();
+    let n = m.height();
 
-    let mut oxy_cands: Vec<&[bool]> = m.iter().map(|l| l.as_ref()).collect();
-    let mut co2_cands: Vec<&[bool]> = oxy_cands.clone();
-    for i in 0..n {
-        let (ones, zeroes) = count(&oxy_cands, i);
+    // Row indexes that are stil in play.
+    let mut oxy_cands: Vec<usize> = (0..n).collect();
+    let mut co2_cands: Vec<usize> = (0..n).collect();
+    for col in 0..n {
+        let (ones, zeroes) = count(&m, &oxy_cands, col);
         let crit = ones >= zeroes;
-        oxy_cands.retain(|l| l[i] == crit);
+        oxy_cands.retain(|&row| m[point(col as isize, row as isize)] == crit);
         if oxy_cands.len() == 1 {
             break;
         }
     }
     assert_eq!(oxy_cands.len(), 1);
-    let oxy_rating = from_base2(oxy_cands[0]);
+    let oxy_rating = from_base2(&m, oxy_cands[0]);
 
-    for i in 0..n {
-        let (ones, zeroes) = count(&co2_cands, i);
+    for col in 0..n {
+        let (ones, zeroes) = count(&m, &co2_cands, col);
         let crit = ones < zeroes;
-        co2_cands.retain(|l| l[i] == crit);
+        co2_cands.retain(|&row| m[point(col as isize, row as isize)] == crit);
         if co2_cands.len() == 1 {
             break;
         }
     }
     assert_eq!(co2_cands.len(), 1);
-    let co2_rating = from_base2(&co2_cands[0]);
+    let co2_rating = from_base2(&m, co2_cands[0]);
     oxy_rating * co2_rating
 }
 
