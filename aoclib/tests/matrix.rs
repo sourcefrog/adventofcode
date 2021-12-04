5ed8e88d344d53bfc695327ac0010d6380da670b
Martin Pool <mbp@sourcefrog.net>
2021-12-04 09:25:26 -0800

    Split out matrix tests

---
+++ aoclib/tests/matrix.rs
@@ -0,0 +1,64 @@
+// Copyright 2020 Google LLC
+// Copyright 2021 Martin Pool
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+use aoclib::{point, Matrix};
+
+#[test]
+fn simple_matrix() {
+    let mut m = Matrix::new(10, 10, 7u8);
+    assert_eq!(m[point(5, 5)], 7u8);
+    m[point(6, 6)] = 10;
+    assert_eq!(m[point(6, 6)], 10);
+    assert_eq!(m[point(5, 5)], 7u8);
+
+    assert_eq!(m.row(4).cloned().collect::<Vec<u8>>(), vec![7u8; 10]);
+}
+
+#[test]
+fn index_by_tuple() {
+    let mut m = Matrix::new(10, 10, 7u8);
+    assert_eq!(m[(5usize, 5)], 7u8);
+    m[point(6, 6)] = 10;
+    assert_eq!(m[(6usize, 6)], 10);
+    assert_eq!(m[(5usize, 5)], 7u8);
+}
+
+#[test]
+fn from_string() {
+    let input = std::fs::read_to_string("testdata/dec11.txt").unwrap();
+    let matrix = Matrix::from_string_lines(&input);
+    assert_eq!(matrix.width(), 93);
+    assert_eq!(matrix.height(), 90);
+}
+
+#[test]
+fn from_file() {
+    let matrix = Matrix::from_file("testdata/dec11.txt");
+    assert_eq!(matrix.width(), 93);
+    assert_eq!(matrix.height(), 90);
+}
+
+#[test]
+fn from_iter_iter() {
+    let matrix: Matrix<usize> = (0..=1)
+        .into_iter()
+        .map(|row| (0..=1).into_iter().map(move |col| row * 10 + col))
+        .collect();
+    assert_eq!((matrix.width(), matrix.height()), (2, 2));
+    assert_eq!(
+        matrix.values().map(|v| *v).collect::<Vec<usize>>(),
+        [0, 1, 10, 11]
+    );
+}
