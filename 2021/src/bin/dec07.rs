a48aae79bd1d80c447d1327de12ba5a3fed50461
Martin Pool <mbp@sourcefrog.net>
2021-12-06 21:09:12 -0800

    Solve 07
    Pretty crude solution; I'm sure these don't need to be summed up the
    hard way but it works...

---
+++ 2021/src/bin/dec07.rs
@@ -0,0 +1,59 @@
+// Copyright 2021 Martin Pool
+
+//! https://adventofcode.com/2021/day/7
+
+fn main() {
+    let input = input();
+    println!("{:?}", solve_a(&input));
+    println!("{:?}", solve_b(&input));
+}
+
+fn input() -> String {
+    std::fs::read_to_string("input/07.txt").unwrap()
+}
+
+fn solve_a(input: &str) -> i64 {
+    let ps = input
+        .trim()
+        .split(',')
+        .map(|w| w.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+    let mut best = i64::MAX;
+    for i in (1..(*ps.iter().max().unwrap())) {
+        let guess = ps.iter().map(|x| (x - i).abs()).sum();
+        best = std::cmp::min(best, guess);
+    }
+    best
+    // 358620 is wrong
+}
+
+fn solve_b(input: &str) -> i64 {
+    let ps = input
+        .trim()
+        .split(',')
+        .map(|w| w.parse::<i64>().unwrap())
+        .collect::<Vec<i64>>();
+    let mut best = i64::MAX;
+    for i in (1..(*ps.iter().max().unwrap())) {
+        let guess = ps
+            .iter()
+            .map(|x| {
+                let d = (x - i).abs();
+                (1..=d).sum::<i64>()
+            })
+            .sum();
+        best = std::cmp::min(best, guess);
+    }
+    best
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn solution() {
+        assert_eq!(solve_a(&input()), 389726);
+        assert_eq!(solve_b(&input()), 1743335992042);
+    }
+}
