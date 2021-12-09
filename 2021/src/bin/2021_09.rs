513bf9eb7ec218d70e57a76919ed5164d9fb7586
Martin Pool <mbp@sourcefrog.net>
2021-12-08 22:05:40 -0800

    Clean up a bit

--- 2021/src/bin/2021_09.rs
+++ 2021/src/bin/2021_09.rs
@@ -12,35 +12,36 @@ fn main() {
 
 fn input() -> String {
     std::fs::read_to_string("input/09.txt").unwrap()
-    // "2199943210
-    // 3987894921
-    // 9856789892
-    // 8767896789
-    // 9899965678"
-    //     .into()
 }
 
 fn solve(input: &str) -> (u32, usize) {
+    // The map as integer heights
     let map = Matrix::from_string_lines(input).map(|c| {
         assert!(c.is_ascii_digit());
         *c as u32 - '0' as u32
     });
     let mut sol_a = 0;
-    let mut low_points: Vec<Point> = Vec::new();
-    // For each point in the map that's not height 9, the downhill neighbors.
+    // For each point in the map that's not height 9 or a low point, its
+    // the downhill neighbors.
     let mut flows_to: Matrix<Vec<Point>> = map.map(|_| Vec::new());
+    // Points that we have to look at, where we haven't decided yet
+    // which basin they're in, if any.
     let mut active: VecDeque<Point> = Default::default();
+    // For points that we have decided which basin they're in: the low
+    // point of that basin.
     let mut in_basin: Matrix<Option<Point>> = map.map(|_| None);
 
-    // For the low points, the
-    let mut basins: HashMap<Point, Vec<Point>> = HashMap::new();
+    // For each basin, identified by its low point: the number of points
+    // in that basin, including the low point.
+    let mut basins: HashMap<Point, usize> = HashMap::new();
+
     for (p, &v) in map.point_values() {
         let ns = map.neighbors4(p);
         if ns.iter().all(|(_np, nv)| **nv > v) {
-            low_points.push(p);
+            // Everything is higher: it's a low point.
             sol_a += v + 1;
             in_basin[p] = Some(p);
-            basins.insert(p, vec![p]);
+            basins.insert(p, 1);
         } else if v < 9 {
             flows_to[p] = ns
                 .iter()
@@ -57,22 +58,22 @@ fn solve(input: &str) -> (u32, usize) {
         let nbr_basins: Vec<Option<Point>> = flows_to[p].iter().map(|np| in_basin[*np]).collect();
         if nbr_basins.iter().all(Option::is_some) {
             // all downhills have been done; are they all the same?
-            let nb = nbr_basins[0].unwrap();
-            if nbr_basins.iter().all(|x| x.unwrap() == nb) {
-                println!("resolved {p} is in basin of {nb}");
-                in_basin[p] = Some(nb);
-                basins.get_mut(&nb).unwrap().push(p);
+            let lowpt = nbr_basins[0].unwrap();
+            if nbr_basins.iter().all(|x| x.unwrap() == lowpt) {
+                // println!("resolved {p} is in basin of {lowpt}");
+                in_basin[p] = Some(lowpt);
+                *basins.get_mut(&lowpt).unwrap() += 1;
             } else {
-                println!("resolved {p} is not in a single basin");
+                // println!("resolved {p} is not in a single basin");
             }
         } else {
-            println!("try again later on {p}");
+            // println!("try again later on {p}");
             active.push_back(p);
         }
     }
 
-    let mut lens: Vec<usize> = basins.values().map(|b| b.len()).collect();
-    lens.sort();
+    let mut lens: Vec<usize> = basins.values().cloned().collect();
+    lens.sort_unstable();
     let sol_b = lens.iter().rev().take(3).product();
 
     (sol_a, sol_b)
@@ -86,5 +87,15 @@ mod test {
     fn solution() {
         let input = input();
         assert_eq!(solve(&input), (600, 987840));
+    }
+
+    #[test]
+    fn example() {
+        let example = "2199943210
+3987894921
+9856789892
+8767896789
+9899965678";
+        assert_eq!(solve(example), (15, 1134));
     }
 }
