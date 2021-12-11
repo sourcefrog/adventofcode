8518f07a32475f5d0b53726be62a031970c1de78
Martin Pool <mbp@sourcefrog.net>
2021-12-10 21:19:09 -0800

    Solve 2021_11b

--- 2021/src/bin/2021_11.rs
+++ 2021/src/bin/2021_11.rs
@@ -56,7 +56,47 @@ fn solve_a(input: &str) -> usize {
 }
 
 fn solve_b(input: &str) -> usize {
-    0
+    let mut m: Matrix<u32> = Matrix::from_string_lines(input).map(|&c| {
+        assert!(c.is_ascii_digit());
+        c as u32 - '0' as u32
+    });
+    let mut tf = 0;
+    for step in 1.. {
+        dbg!(&step);
+        let mut n = m.map(|&v| v + 1);
+        let mut flashed = m.map(|_| false);
+        loop {
+            // println!(
+            //     "{}",
+            //     n.map(|&v| "0123456789abcdefghijk".chars().nth(v as usize).unwrap())
+            // );
+            let mut done = true;
+            for p in n.iter_points() {
+                if n[p] > 9 && !flashed[p] {
+                    flashed[p] = true;
+                    tf += 1;
+                    println!("{} flashed", p);
+                    for q in n.neighbor8_points(p) {
+                        n[q] += 1;
+                        done = false;
+                    }
+                }
+            }
+            if done {
+                break;
+            }
+        }
+        for p in n.iter_points() {
+            if flashed[p] {
+                n[p] = 0;
+            }
+        }
+        m = n;
+        if flashed.values().all(|&x| x) {
+            return step;
+        }
+    }
+    unreachable!()
 }
 
 #[cfg(test)]
