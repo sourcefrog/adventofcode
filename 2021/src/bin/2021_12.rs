f04670fc3845c6b59dade5440d7effd94aa0cc05
Martin Pool <mbp@sourcefrog.net>
2021-12-11 21:13:14 -0800

    2021_12a

---
+++ 2021/src/bin/2021_12.rs
@@ -0,0 +1,67 @@
+// Copyright 2021 Martin Pool
+
+//! https://adventofcode.com/2021/day/12
+
+use aoclib::Matrix;
+use std::collections::BTreeMap;
+
+fn main() {
+    let input = input();
+    // let input = "start-A
+    // start-b
+    // A-c
+    // A-b
+    // b-d
+    // A-end
+    // b-end
+    // ";
+    println!("{:?}", solve(&input));
+}
+
+fn input() -> String {
+    std::fs::read_to_string("input/12.txt").unwrap()
+}
+
+fn solve(input: &str) -> (usize, usize) {
+    let mut from: BTreeMap<&str, Vec<&str>> = BTreeMap::new();
+    input
+        .lines()
+        .map(|l| l.split_once('-').unwrap())
+        .for_each(|(a, b)| {
+            from.entry(a).or_default().push(b);
+            from.entry(b).or_default().push(a);
+        });
+    dbg!(&from);
+    let mut inc: Vec<Vec<&str>> = vec![vec!["start"]];
+    let mut compl: Vec<Vec<&str>> = Vec::new();
+    while let Some(p) = inc.pop() {
+        let last = p.last().unwrap();
+        for next in &from[last] {
+            if p.contains(next) && next.chars().all(|c| c.is_ascii_lowercase()) {
+                continue;
+            }
+            let mut q = p.clone();
+            q.push(next);
+
+            if *next == "end" {
+                println!("found {q:?}");
+                compl.push(q);
+            } else {
+                inc.push(q);
+            }
+        }
+    }
+
+    (compl.len(), 0)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn solution() {
+        let input = input();
+        assert_eq!(solve(&input), (0, 0));
+    }
+}
