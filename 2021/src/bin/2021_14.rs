6fb5a81526cd6074e7d1d7db4be9879f5ab24d75
Martin Pool <mbp@sourcefrog.net>
2021-12-13 21:48:30 -0800

    work

--- 2021/src/bin/2021_14.rs
+++ 2021/src/bin/2021_14.rs
@@ -19,26 +19,58 @@ fn input() -> String {
 }
 
 fn count(s: &[char], c: char) -> Option<usize> {
-let n = s.iter().filter(|&&x| x == c).count();
-if n > 0 {Some(n)} else {None}
+    let n = s.iter().filter(|&&x| x == c).count();
+    if n > 0 {Some(n)} else {None}
 }
 
 fn solve(input: &str) -> (usize, usize) {
     let template: Vec<char> = input.lines().next().unwrap().chars().collect();
-    let from = parse_map(input);
-    dbg!(&from);
+    let m = parse_map(input);
+    dbg!(&m);
 
     let mut s = template.to_owned();
     for step in 1..=10 {
-        println!("{}", s.iter().collect::<String>());
-        println!("step {step} len {}", s.len());
-        s = expand(&s, &from);
+        // println!("{}", s.iter().collect::<String>());
+        println!("step {step:2} len {:10} F={:8} O={:8}", s.len(), count(&s, 'F').unwrap(), count(&s, 'O').unwrap());
+        s = expand(&s, &m);
     }
 
-    let sol_a = ('A'..='Z').into_iter().flat_map(|c| count(&s, c)).max().unwrap()
-        - ('A'..='Z').into_iter().flat_map(|c| count(&s, c)).min().unwrap();
+    for c in 'A'..='Z' {
+        if let Some(n) = count(&s,c) {
+            let rn = m.values().filter(|&&x| x == c).count();
+            println!("{c} {:8} {:8}", n, rn);
+        }
+    }
+
+    let most_common_count = ('A'..='Z').into_iter().flat_map(|c| count(&s, c)).max().unwrap();
+    let least_common_count = ('A'..='Z').into_iter().flat_map(|c| count(&s, c)).min().unwrap();
+    let sol_a = most_common_count - least_common_count;
+
+    // So this is also like matrix exponentiation... One initial pair will produce an expanding 
+    // series.... 
+    // if we look at just the first O it cycles OO-OKO then OK -> OFK... then OF -> OC... 
+    // then OC -> OHC ...
+
+    // I wonder if every pair in the input is in the expansion and if every produced 
+    // expansion also expands?
+
+    // It seems so; every time we add len-1 elements - it *almost* doubles.
+
+    // We could say, which one is added by the most rules but that's probably too simple...
+    // Although it does seem true here...
+
+    let s = template.clone();
+    for i in 0..(s.len()-1) {
+        assert!(m.contains_key(&[s[i], s[i+1]]));
+    }
+    for (k,&v) in &m {
+        let nk = [k[0], v];
+        assert!(m.contains_key(&nk));
+    }
+
+
     let sol_b = 0;
-    // 3074 is wrong
+
     (sol_a, sol_b)
 }
 
@@ -106,7 +138,7 @@ CN -> C
     #[test]
     fn solution() {
         let (a, b) = solve(&input());
-        assert_eq!(a, 0);
+        assert_eq!(a, 2194);
         assert_eq!(b, 0);
     }
 }
