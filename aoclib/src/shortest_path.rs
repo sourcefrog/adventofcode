cf3b8b19d61193d7609649411cd084e42a858be4
Martin Pool <mbp@sourcefrog.net>
2022-01-01 11:11:38 -0800

    Add A* (unused so far)

--- aoclib/src/shortest_path.rs
+++ aoclib/src/shortest_path.rs
@@ -41,6 +41,62 @@ where
     P: Eq + Ord + Clone + Hash + Debug,
     D: Ord + Add<Output = D> + Clone + Default + Debug,
 {
+    /// Find the shortest path using
+    /// [A* search](https://en.wikipedia.org/wiki/A*_search_algorithm).
+    ///
+    /// `origin` is the starting point.
+    ///
+    /// `estimate` returns a *lower bound* on the distance between a candidate
+    /// point `p` and the destination. This must be `0` (more precisely, `D::default()`)
+    /// for the destination, and greater than `0` for every other point.
+    pub fn find_astar<NbrFn, EstFn>(origin: &P, estimate: EstFn, neighbors: NbrFn) -> Self
+    where
+        NbrFn: Fn(&P) -> Vec<(P, D)>,
+        EstFn: Fn(&P) -> D,
+    {
+        // Next points to visit, indexed by distance so far.
+        let mut queue = MinHeap::<(D, P)>::new();
+        // Shortest known distance to reach any point.
+        let mut best = HashMap::<P, D>::new();
+        // The previous state that leads, on the best path, to this state.
+        let mut predecessor = std::collections::BTreeMap::<P, P>::new();
+        queue.push((Default::default(), origin.clone()));
+        best.insert(origin.clone(), Default::default());
+        loop {
+            let (d, p) = queue
+                .pop()
+                .expect("heap is empty without reaching destination");
+            let est = estimate(&p);
+            if est == D::default() {
+                // Reassemble (a) shortest path to the destination by looking backwards
+                // at the step that led to each point.
+                let mut path = vec![p];
+                while let Some(next) = predecessor.get(path.last().unwrap()) {
+                    path.push(next.clone());
+                }
+                path.reverse();
+                return ShortestPath { distance: d, path };
+            }
+            debug_assert!(
+                est > D::default(),
+                "estimate {:?} of {:?} is negative",
+                est,
+                &p
+            );
+            for (np, step) in neighbors(&p) {
+                let nd = step + d.clone();
+                if let Some(prev_d) = best.get(&np) {
+                    if nd >= *prev_d {
+                        continue; // Already found a shorter path; don't revisit.
+                    }
+                }
+                best.insert(np.clone(), nd.clone());
+                queue.push((nd + est.clone(), np.clone()));
+                predecessor.insert(np, p.clone());
+            }
+        }
+    }
+
     /// Find the shortest path in a graph, using Djikstra's method.
     ///
     /// `origin` is the starting point.
