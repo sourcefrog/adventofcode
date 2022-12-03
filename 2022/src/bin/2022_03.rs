f99f5ceba797b50ee7e168c99a32c9423d584568
Martin Pool <mbp@sourcefrog.net>
2022-12-02 21:26:25 -0800

    Simplify a bit

--- 2022/src/bin/2022_03.rs
+++ 2022/src/bin/2022_03.rs
@@ -17,46 +17,32 @@ fn solve_a(input: &str) -> usize {
         assert!(ll % 2 == 0);
         let a = &l[0..(ll / 2)];
         let b = &l[(ll / 2)..];
-        let mut found = None;
-        'ca: for ca in a {
-            for cb in b {
-                if ca == cb {
-                    assert!(
-                        found.is_none() || found == Some(*ca),
-                        "found twice in {ls} {ca} {cb}"
-                    );
-                    found = Some(*ca);
-                    sum += pri(*ca);
-                    break 'ca;
-                }
+        for ca in a {
+            if b.contains(ca) {
+                sum += pri(*ca);
+                break;
             }
         }
-        assert!(found.is_some());
-        assert_eq!(a.len(), b.len());
     }
     sum
 }
 
 fn pri(ca: u8) -> usize {
-    match ca {
-        b'a'..=b'z' => (ca - b'a' + 1) as usize,
-        b'A'..=b'Z' => (ca - b'A' + 27) as usize,
-        x => panic!("unexpected char {}", x),
-    }
+    (if ca.is_ascii_lowercase() {
+        (ca - b'a') + 1
+    } else {
+        (ca - b'A') + 27
+    }) as usize
 }
 
 fn solve_b(input: &str) -> usize {
     let mut sum = 0;
     for ll in input.lines().collect::<Vec<&str>>().chunks(3) {
-        'chars: for c in ('a'..='z').chain('A'..='Z') {
-            let c = c as u8;
-            for l in ll {
-                if !l.as_bytes().contains(&c) {
-                    continue 'chars;
-                }
+        for ca in ll[0].as_bytes() {
+            if ll[1].as_bytes().contains(ca) && ll[2].as_bytes().contains(ca) {
+                sum += pri(*ca);
+                break;
             }
-            sum += pri(c);
-            break;
         }
     }
     sum
