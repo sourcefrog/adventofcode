d13f36086a080274cd92c34f12e16cc1533342a2
Martin Pool <mbp@sourcefrog.net>
2022-12-03 21:12:56 -0800

    Tidy up

--- 2022/src/bin/2022_04.rs
+++ 2022/src/bin/2022_04.rs
@@ -1,38 +1,36 @@
 //! https://adventofcode.com/2022/day/
 
 fn main() {
-    println!("{}", solve_a(&input()));
-    println!("{}", solve_b(&input()));
+    let (a, b) = solve(&input());
+    println!("{a}");
+    println!("{b}");
 }
 
 fn input() -> String {
     std::fs::read_to_string("input/04.txt").unwrap()
 }
 
-fn solve_a(input: &str) -> usize {
-    let mut count = 0;
+fn solve(input: &str) -> (usize, usize) {
+    let mut enclosed = 0;
+    let mut overlap = 0;
     for l in input.lines() {
         let n: Vec<usize> = l
             .split(|c: char| !c.is_ascii_digit())
             .map(|s| s.parse::<usize>().unwrap())
             .collect();
         assert_eq!(n.len(), 4);
-        // if (n[0] <= n[2] && n[1] >= n[3]) || (n[2] <= n[0] && n[3] >= n[1]) {
-        //     count += 1;
-        // }
-        if (n[1] >= n[2] && n[1] <= n[3])
-            || (n[0] >= n[2] && n[0] <= n[3])
+        if (n[0] <= n[2] && n[1] >= n[3]) || (n[2] <= n[0] && n[3] >= n[1]) {
+            enclosed += 1;
+        }
+        if (n[0] >= n[2] && n[0] <= n[3])
+            || (n[1] >= n[2] && n[1] <= n[3])
             || (n[2] >= n[0] && n[2] <= n[1])
             || (n[3] >= n[0] && n[3] <= n[1])
         {
-            count += 1
+            overlap += 1
         }
     }
-    count
-}
-
-fn solve_b(input: &str) -> usize {
-    0
+    (enclosed, overlap)
 }
 
 #[cfg(test)]
@@ -40,12 +38,7 @@ mod test {
     use super::*;
 
     #[test]
-    fn solution_a() {
-        assert_eq!(solve_a(&input()), 0);
-    }
-
-    #[test]
-    fn solution_b() {
-        assert_eq!(solve_b(&input()), 0);
+    fn solution() {
+        assert_eq!(solve(&input()), (433, 852));
     }
 }
