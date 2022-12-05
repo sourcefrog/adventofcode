87be1e2dc6910fc0a899a4fe65cfc3226a6b009a
Martin Pool <mbp@sourcefrog.net>
2022-12-04 21:20:58 -0800

    Clean up

--- 2022/src/bin/2022_05.rs
+++ 2022/src/bin/2022_05.rs
@@ -9,7 +9,7 @@ fn input() -> String {
     std::fs::read_to_string("input/05.txt").unwrap()
 }
 
-fn solve_a(input: &str) -> String {
+fn parse(input: &str) -> (Vec<Vec<char>>, Vec<(usize, usize, usize)>) {
     let mut stacks: Vec<Vec<char>> = vec![vec![]; 9];
     let mut lines = input.lines();
     while let Some(l) = lines.next() {
@@ -25,23 +25,37 @@ fn solve_a(input: &str) -> String {
         }
     }
     assert!(lines.next().unwrap().trim().is_empty());
+    let mut instructions = Vec::new();
     while let Some(l) = lines.next() {
         let w: Vec<&str> = l.split_ascii_whitespace().collect();
         let cnt: usize = w[1].parse().unwrap();
         let from_i: usize = w[3].parse::<usize>().unwrap() - 1;
         let to_i: usize = w[5].parse::<usize>().unwrap() - 1;
-        // for _ in 0..cnt {
+        instructions.push((cnt, from_i, to_i))
+    }
+    (stacks, instructions)
+}
+
+fn solve_a(input: &str) -> String {
+    let (mut stacks, instructions) = parse(input);
+    for (cnt, from_i, to_i) in instructions {
+        for _ in 0..cnt {
+            let x = stacks[from_i].pop().unwrap();
+            stacks[to_i].push(x);
+        }
+    }
+    stacks.iter().map(|s| s.last().unwrap()).collect()
+}
+
+fn solve_b(input: &str) -> String {
+    let (mut stacks, instructions) = parse(input);
+    for (cnt, from_i, to_i) in instructions {
         let j = stacks[from_i].len() - cnt;
         let mut x = stacks[from_i].split_off(j);
         assert_eq!(x.len(), cnt);
         stacks[to_i].append(&mut x)
     }
-
     stacks.iter().map(|s| s.last().unwrap()).collect()
-}
-
-fn solve_b(input: &str) -> usize {
-    0
 }
 
 #[cfg(test)]
@@ -50,11 +64,11 @@ mod test {
 
     #[test]
     fn solution_a() {
-        assert_eq!(solve_a(&input()), 0);
+        assert_eq!(solve_a(&input()), "VPCDMSLWJ");
     }
 
     #[test]
     fn solution_b() {
-        assert_eq!(solve_b(&input()), 0);
+        assert_eq!(solve_b(&input()), "TPWCGNCCG");
     }
 }
