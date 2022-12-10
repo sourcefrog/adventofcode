be0d91c4671eea4522aab7a34e51410c4d301591
Martin Pool <mbp@sourcefrog.net>
2022-12-09 21:34:27 -0800

    Tidy up

--- 2022/src/bin/2022_10.rs
+++ 2022/src/bin/2022_10.rs
@@ -11,14 +11,14 @@ fn input() -> String {
     std::fs::read_to_string("input/10.txt").unwrap()
 }
 
-fn m(cycle: isize) -> bool {
-    match cycle {
-        20 | 60 | 100 | 140 | 180 | 220 => true,
-        _ => false,
-    }
-}
-
 fn solve_a(input: &str) -> isize {
+    fn m(cycle: isize) -> bool {
+        match cycle {
+            20 | 60 | 100 | 140 | 180 | 220 => true,
+            _ => false,
+        }
+    }
+
     let mut x = 1;
     let mut cycle = 1isize;
     let mut tot = 0;
@@ -38,31 +38,34 @@ fn solve_a(input: &str) -> isize {
     tot
 }
 
-fn hit(x: isize, cycle: isize, display: &mut Matrix<bool>) {
-    if cycle > 240 {
-        return;
-    }
-    if (x - ((cycle - 1) % 40)).abs() < 2 {
-        let dx = (cycle - 1) % 40;
-        let dy = (cycle - 1) / 40;
-        display[(dx, dy)] = true;
-    }
-}
-
 fn solve_b(input: &str) -> String {
+    fn hit(x: isize, cycle: isize, display: &mut String) {
+        if cycle > 240 {
+            return;
+        }
+        if (x - ((cycle - 1) % 40)).abs() < 2 {
+            display.push('#')
+        } else {
+            display.push('.')
+        }
+        if (cycle - 1) % 40 == 39 {
+            display.push('\n');
+        }
+    }
+
     let mut x = 1;
     let mut cycle = 1isize;
-    let mut display = Matrix::new(40, 6, false);
+    let mut dis = String::new();
     for l in input.lines() {
-        hit(x, cycle, &mut display);
+        hit(x, cycle, &mut dis);
         cycle += 1;
         if let Some(v) = l.strip_prefix("addx ") {
-            hit(x, cycle, &mut display);
+            hit(x, cycle, &mut dis);
             cycle += 1;
             x += v.parse::<isize>().unwrap();
         }
     }
-    display.to_string_lines()
+    dis
 }
 
 #[cfg(test)]
