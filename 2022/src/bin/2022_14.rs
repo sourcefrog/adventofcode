5b8cb69e65968b1810c9113e4c74a0ecaa73d1aa
Martin Pool <mbp@sourcefrog.net>
2022-12-13 21:32:13 -0800

    Factor out load

--- 2022/src/bin/2022_14.rs
+++ 2022/src/bin/2022_14.rs
@@ -13,35 +13,46 @@ fn input() -> String {
     std::fs::read_to_string("input/14.txt").unwrap()
 }
 
-fn solve_a(input: &str) -> usize {
-    let mut mat = Matrix::new(600, 600, '.');
+fn load(input: &str) -> Matrix<char> {
+    let mut draw: Vec<Vec<Point>> = Vec::new();
     for l in input.lines() {
-        let mut last: Option<Point> = None;
+        draw.push(Vec::new());
         for ps in l.split(" -> ") {
             let (x, y) = ps.split_once(',').unwrap();
             let x: isize = x.parse().unwrap();
             let y: isize = y.parse().unwrap();
-            let p = point(x, y);
-            if let Some(last) = last {
-                if last.x == x {
-                    for iy in min(last.y, y)..=max(last.y, y) {
-                        mat[point(x, iy)] = '#';
+            draw.last_mut().unwrap().push(point(x, y));
+        }
+    }
+    let mut mat = Matrix::new(1200, 600, '.');
+    for shape in draw {
+        for pair in shape.windows(2) {
+            if let [p1, p2] = pair {
+                if p1.x == p2.x {
+                    for iy in min(p1.y, p2.y)..=max(p1.y, p2.y) {
+                        mat[point(p1.x, iy)] = '#';
                     }
-                } else if last.y == y {
-                    for ix in min(last.x, x)..=max(last.x, x) {
-                        mat[point(ix, y)] = '#';
+                } else if p1.y == p2.y {
+                    for ix in min(p1.x, p2.x)..=max(p1.x, p2.x) {
+                        mat[point(ix, p1.y)] = '#';
                     }
                 } else {
                     panic!()
                 }
+            } else {
+                unreachable!()
             }
-            last = Some(p);
         }
     }
+    mat
+}
+
+fn solve_a(input: &str) -> usize {
+    let mut mat = load(input);
     let tap = point(500, 0);
     's: while mat[tap] == '.' {
         let mut sp = tap;
-        'q: loop {
+        loop {
             if (sp.y + 1) >= mat.height() as isize {
                 // fell off the bottom
                 break 's;
@@ -65,7 +76,34 @@ fn solve_a(input: &str) -> usize {
 }
 
 fn solve_b(input: &str) -> usize {
-    input.len()
+    let mut mat = load(input);
+    let mut bottom = mat.find_values(&'#').map(|p| p.y).max().unwrap();
+    bottom += 1;
+    let tap = point(500, 0);
+    's: while mat[tap] == '.' {
+        let mut sp = tap;
+        loop {
+            if sp.y == bottom {
+                assert_eq!(mat[sp], '.');
+                // println!("bottom at {sp:?}");
+                mat[sp] = 'o';
+                continue 's;
+            } else if mat[sp.down()] == '.' {
+                sp = sp.down();
+            } else if mat[sp.down().left()] == '.' {
+                sp = sp.down().left();
+            } else if mat[sp.down().right()] == '.' {
+                sp = sp.down().right();
+            } else {
+                // can't move
+                assert_eq!(mat[sp], '.');
+                // println!("fill {sp:?}");
+                mat[sp] = 'o';
+                continue 's;
+            }
+        }
+    }
+    mat.values().filter(|c| **c == 'o').count()
 }
 
 #[cfg(test)]
@@ -74,11 +112,11 @@ mod test {
 
     #[test]
     fn solution_a() {
-        assert_eq!(solve_a(&input()), 9900);
+        assert_eq!(solve_a(&input()), 1133);
     }
 
     #[test]
     fn solution_b() {
-        assert_eq!(solve_b(&input()), 9900);
+        assert_eq!(solve_b(&input()), 27566);
     }
 }
