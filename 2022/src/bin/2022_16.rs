1ccce4b98383e2b2fb836e23516ac7d6a468738d
Martin Pool <mbp@sourcefrog.net>
2022-12-18 16:54:40 -0800

    Restore part 1 solution; don't need to sit

--- 2022/src/bin/2022_16.rs
+++ 2022/src/bin/2022_16.rs
@@ -8,7 +8,7 @@ use itertools::Itertools;
 use regex::Regex;
 
 fn main() {
-    // println!("{}", solve_a(&input()));
+    println!("{}", solve_a(&input()));
     println!("{}", solve_b(&input()));
     // println!("{}", solve_b(EX));
     // println!("{}", solve_b(&input()));
@@ -51,11 +51,6 @@ impl<'a> St<'a> {
             ..self.clone()
         }
     }
-
-    #[must_use]
-    fn sit(&self) -> St<'a> {
-        St { ..self.clone() }
-    }
 }
 
 fn parse(input: &str) -> BTreeMap<&str, Valve> {
@@ -78,6 +73,81 @@ fn parse(input: &str) -> BTreeMap<&str, Valve> {
         vs.insert(name, Valve { rate, tun });
     }
     vs
+}
+
+#[derive(Debug)]
+struct Opt {
+    opened: Vec<String>,
+    seen: Vec<String>,
+    flow: usize,
+    rem: usize,
+}
+
+fn solve_a(input: &str) -> usize {
+    let mut vs: BTreeMap<String, Valve> = BTreeMap::new();
+    let re =
+        Regex::new(r"Valve (..) has flow rate=([0-9]+); tunnels? leads? to valves? ([ ,A-Z]*)")
+            .unwrap();
+    for l in input.lines() {
+        dbg!(l);
+        let caps = re.captures(l).unwrap();
+        let name = caps.get(1).unwrap().as_str().to_string();
+        let rate = caps.get(2).unwrap().as_str().parse::<usize>().unwrap();
+        let tun = caps
+            .get(3)
+            .unwrap()
+            .as_str()
+            .split(", ")
+            .map(|s| s.to_owned())
+            .collect();
+        vs.insert(name, Valve { rate, tun });
+    }
+    let mut opts = vec![Opt {
+        opened: Vec::new(),
+        seen: vec!["AA".into()],
+        flow: 0,
+        rem: 30,
+    }];
+    let mut best = 0;
+    while let Some(o) = opts.pop() {
+        // println!("{o:?}");
+        let loc = o.seen.last().unwrap().clone();
+        let here = &vs[&loc];
+        best = std::cmp::max(best, o.flow);
+        if o.rem == 0 {
+            continue;
+        }
+        if !o.opened.contains(&loc) && here.rate > 0 {
+            // open this valve
+            let mut opened = o.opened.clone();
+            opened.push(loc.clone());
+            let rem = o.rem - 1;
+            opts.push(Opt {
+                opened,
+                rem,
+                flow: o.flow + (here.rate * rem),
+                seen: o.seen.clone(),
+            })
+        }
+        for n in &here.tun {
+            if o.seen.contains(&n) {
+                continue;
+            }
+            let mut seen = o.seen.clone();
+            seen.push(n.clone());
+            // if !all_seen.insert(seen.clone()) {
+            //     continue;
+            // };
+            opts.push(Opt {
+                opened: o.opened.clone(),
+                rem: o.rem - 1,
+                seen,
+                flow: o.flow,
+                ..o
+            })
+        }
+    }
+    best
 }
 
 fn solve_b(input: &str) -> usize {
@@ -108,7 +178,8 @@ fn solve_b(input: &str) -> usize {
                 .map(|(_, v)| v)
                 .sum();
             let mut a = vec![st.clone()];
-            // each actor can either open a valve at their current location (if there is one) or move to any neighboring location or stay still
+            // each actor can either open a valve at their current location (if
+            // there is one) or move to any neighboring location or stay still
             // generate the combinations of all these moves
             for i in 0..2 {
                 let mut b = Vec::new();
@@ -120,7 +191,8 @@ fn solve_b(input: &str) -> usize {
                     for neig in &vs[p].tun {
                         b.push(s.move_to(i, neig));
                     }
-                    b.push(s.sit());
+                    // No need to generate sit-still states, because we can just dance around if
+                    // there are no more useful moves.
                 }
                 a = b;
             }
@@ -161,10 +233,10 @@ mod test {
         assert_eq!(solve_b(&EX), 1707);
     }
 
-    // #[test]
-    // fn solution_a() {
-    //     assert_eq!(solve_a(&input()), 9900);
-    // }
+    #[test]
+    fn solution_a() {
+        assert_eq!(solve_a(&input()), 1617);
+    }
 
     #[test]
     fn solution_b() {
