39b760c4a98f9a722d28ff6c07b4571c5c6068e2
Martin Pool <mbp@sourcefrog.net>
2022-12-20 18:37:06 -0800

    Maybe better approach

--- 2022/src/bin/2022_20.rs
+++ 2022/src/bin/2022_20.rs
@@ -24,82 +24,101 @@ fn input() -> String {
     std::fs::read_to_string("input/20.txt").unwrap()
 }
 
-/// There is one El for each element in the input; they remain in a vec
-/// corresponding to the order they occur in the input.
-#[derive(Eq, PartialEq, Debug)]
-struct El {
-    /// The integer value contained.
-    val: isize,
-    /// The current position in the ring.
-    pos: usize,
-}
+/// A permutation of the elements of an input of given size.
+#[derive(PartialEq, Eq, Debug)]
+struct Perm(Vec<usize>);
 
-struct Ring {
-    els: Vec<El>,
-}
+impl Perm {
+    /// Make a new permutation of `len` elements.
+    fn new(len: usize) -> Perm {
+        Perm((0..len).collect())
+    }
 
-impl Ring {
-    fn new(vals: &[isize]) -> Ring {
-        Ring {
-            els: vals
-                .iter()
-                .copied()
-                .enumerate()
-                .map(|(pos, val)| El { pos, val })
-                .collect_vec(),
+    /// Check this is a well-formed permutation: every element up to the length
+    /// is represented once.
+    fn check(&self) {
+        for i in 0..self.len() {
+            assert!(self.0.contains(&i), "element {i} missing from {:?}", self.0);
         }
     }
 
-    fn check(&self) {
-        for pos in 0..self.els.len() {
-            assert_eq!(self.els.iter().filter(|el| el.pos == pos).count(), 1);
+    fn as_slice(&self) -> &[usize] {
+        &self.0
+    }
+
+    /// Move the element currently at position `x` by `s` elements to the right,
+    /// or to the left if negative.
+    #[must_use]
+    fn move_element(&self, x: usize, mut s: isize) -> Perm {
+        let l = self.0.len();
+        assert!(x < self.0.len());
+        let mut v = self.0.clone();
+        let mut y = x as isize + s;
+        while y < 0 {
+            y += l as isize;
         }
+        y %= l as isize;
+        assert!(y >= 0 && y < l as isize);
+        let y = y as usize;
+        if y >= x {
+            // The input is:         aaaa x bbbb cccc
+            // The output should be: aaaa bbbb y cccc
+            // This means: the positions
+            // Any of a and b might be empty.
+            // This means the position of everything in b is reduced by 1.
+            // Then x takes position y.
+            // The range of y is: (x+1)..=y.
+            for i in (x + 1)..=y {
+                v[i] -= 1;
+            }
+            v[x] = y;
+        } else {
+            // The input is aaa bbb x ccc
+            // We want: aaa y bbb ccc
+            // Everything in bbb moves one position right.
+            for i in y..x {
+                v[i] += 1;
+            }
+            v[x] = y;
+        }
+        let o = Perm(v);
+        o.check();
+        o
     }
 
     fn len(&self) -> usize {
-        self.els.len()
+        self.0.len()
     }
 
-    /// Move the element with the given identity (*not* position) the number of
-    /// values forward or back corresponding to its position.
-    fn rotate(&mut self, i: usize) {
-        let l = self.len() as isize;
-        // How much to move?
-        let mut m = self.els[i].val;
-        println!("move {:?}", &self.els[i]);
-        // First for simplicity convert every rotation to rotation of no less
-        // than one full cycle to the right.
-        while m < 0 {
-            m += l;
+    /// Reorder the elements of a slice according to this permutation.
+    #[must_use]
+    fn apply<T: Clone>(&self, s: &[T]) -> Vec<T> {
+        assert_eq!(s.len(), self.0.len());
+        let mut v = Vec::with_capacity(self.len());
+        for i in 0..(self.0.len()) {
+            // Which element of the input comes next?
+            let j = self.0.iter().position(|y| *y == i).unwrap();
+            v.push(s[j].clone());
         }
-        m %= l;
-        // m now moves it by no more than one cycle relative to its current
-        // position, but now there are two possibilities: it moves it
-        // further right in the array without wrapping around, or
-        self.check();
-        // Now there are two possibilities. We might be moving els[i]
+        v
     }
 }
 
-impl fmt::Debug for Ring {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let mut l = f.debug_list();
-        for pos in 0..self.els.len() {
-            l.entry(&self.els.iter().find(|el| el.pos == pos).unwrap().val);
-        }
-        l.finish()
-    }
-}
+/// Permute the elements of `a` by moving element `x` by `s` elements to the right
+/// (or to the left if negative.)
+fn move_perm(a: &[usize], x: usize, s: usize) {}
 
 fn solve_a(input: &str) -> isize {
     let n = input
         .lines()
         .map(|l| l.trim().parse::<isize>().unwrap())
         .collect_vec();
-    let mut ring = Ring::new(&n);
-    for i in 0..ring.len() {
-        ring.rotate(i);
-        println!("{ring:?}");
+    let mut perm = Perm::new(n.len());
+    for i in 0..n.len() {
+        // TODO: Find which current position corresponds to originally i.
+        // perm = perm.move_element(i)
+        // ring.rotate(i);
+        // println!("{ring:?}");
     }
     // let els: Vec<El> = n
     //     .iter()
@@ -136,6 +155,62 @@ fn solve_a(input: &str) -> isize {
 #[cfg(test)]
 mod test {
     use super::*;
+
+    #[test]
+    fn move_zero() {
+        let p1 = Perm::new(5);
+        assert_eq!(p1.as_slice(), [0, 1, 2, 3, 4]);
+        let p2 = p1.move_element(2, 0);
+        assert_eq!(p2.as_slice(), [0, 1, 2, 3, 4]);
+        assert_eq!(
+            p2.apply(&[100, 101, 102, 103, 104]).as_slice(),
+            [100, 101, 102, 103, 104]
+        );
+    }
+
+    #[test]
+    fn move_right() {
+        let p1 = Perm::new(5);
+        assert_eq!(p1.as_slice(), [0, 1, 2, 3, 4]);
+        let p2 = p1.move_element(2, 2);
+        assert_eq!(p2.as_slice(), [0, 1, 4, 2, 3]);
+        assert_eq!(
+            p2.apply("abcde".chars().collect_vec().as_slice()),
+            "abdec".chars().collect_vec().as_slice(),
+        );
+    }
+
+    #[test]
+    fn move_right_rotate() {
+        let p1 = Perm::new(5);
+        for i in 0..5 {
+            for m in 0..4 {
+                assert_eq!(p1.move_element(i, m * 5), p1);
+            }
+        }
+    }
+
+    #[test]
+    fn move_left_rotate() {
+        let p1 = Perm::new(5);
+        for i in 0..5 {
+            for m in 0..4 {
+                assert_eq!(p1.move_element(i, m * -5), p1);
+            }
+        }
+    }
+
+    #[test]
+    fn move_left() {
+        let p1 = Perm::new(5);
+        assert_eq!(p1.as_slice(), [0, 1, 2, 3, 4]);
+        let p2 = p1.move_element(2, -1);
+        assert_eq!(p2.as_slice(), [0, 2, 1, 3, 4]);
+        assert_eq!(
+            p2.apply("abcde".chars().collect_vec().as_slice()),
+            "acbde".chars().collect_vec().as_slice(),
+        );
+    }
 
     #[test]
     fn ex1() {
