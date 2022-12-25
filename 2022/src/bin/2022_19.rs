14b9b9f1cdf7029a60f9dff56cea10dfdf8c2132
Martin Pool <mbp@sourcefrog.net>
2022-12-24 17:24:26 -0800

    Add the docs

--- 2022/src/bin/2022_19.rs
+++ 2022/src/bin/2022_19.rs
@@ -1,4 +1,73 @@
-//! https://adventofcode.com/2022/day/19
+/*!
+
+# Day 19: Not Enough Minerals
+
+<https://adventofcode.com/2022/day/19>
+
+Your scans show that the lava did indeed form obsidian!
+
+The wind has changed direction enough to stop sending lava droplets toward
+you, so you and the elephants exit the cave. As you do, you notice a collection
+of geodes around the pond. Perhaps you could use the obsidian to create some
+geode-cracking robots and break them open?
+
+To collect the obsidian from the bottom of the pond, you'll need waterproof
+obsidian-collecting robots. Fortunately, there is an abundant amount of clay
+nearby that you can use to make them waterproof.
+
+In order to harvest the clay, you'll need special-purpose clay-collecting
+robots. To make any type of robot, you'll need ore, which is also plentiful but
+in the opposite direction from the clay.
+
+Collecting ore requires ore-collecting robots with big drills. Fortunately,
+you have exactly one ore-collecting robot in your pack that you can use to
+kickstart the whole operation.
+
+Each robot can collect 1 of its resource type per minute. It also takes one
+minute for the robot factory (also conveniently from your pack) to construct
+any type of robot, although it consumes the necessary resources available when
+construction begins.
+
+The robot factory has many blueprints (your puzzle input) you can choose from,
+but once you've configured it with a blueprint, you can't change it. You'll
+need to work out which blueprint is best.
+
+(example)
+
+Determine the quality level of each blueprint by multiplying that blueprint's
+ID number with the largest number of geodes that can be opened in 24 minutes
+using that blueprint. In this example, the first blueprint has ID 1 and can
+open 9 geodes, so its quality level is 9. The second blueprint has ID 2 and can
+open 12 geodes, so its quality level is 24. Finally, if you add up the quality
+levels of all of the blueprints in the list, you get 33.
+
+Determine the quality level of each blueprint using the largest number of
+geodes it could produce in 24 minutes. What do you get if you add up the
+quality level of all of the blueprints in your list?
+
+## Part Two
+
+While you were choosing the best blueprint, the elephants found some food on
+their own, so you're not in as much of a hurry; you figure you probably have 32
+minutes before the wind changes direction again and you'll need to get out of
+range of the erupting volcano.
+
+Unfortunately, one of the elephants ate most of your blueprint list! Now, only
+the first three blueprints in your list are intact.
+
+In 32 minutes, the largest number of geodes blueprint 1 (from the example
+above) can open is 56. One way to achieve that is:
+(example)
+
+You no longer have enough blueprints to worry about quality levels. Instead,
+for each of the first three blueprints, determine the largest number of geodes
+you could open; then, multiply these three values together.
+
+Don't worry about quality levels; instead, just determine the largest number of
+geodes you could open using each of the first three blueprints. What do you get
+if you multiply these numbers together?
+
+*/
 
 #![allow(dead_code, unused_imports)]
 
