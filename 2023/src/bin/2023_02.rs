55247c9de661ee25578a8e85d0a4cd5bbabbf37f
Martin Pool <mbp@sourcefrog.net>
2023-12-01 21:27:25 -0800

    Tidy up 2023_02

--- 2023/src/bin/2023_02.rs
+++ 2023/src/bin/2023_02.rs
@@ -1,5 +1,7 @@
 use std::fs::read_to_string;
 
+use regex::Regex;
+
 fn main() {
     println!("2023_01 a {}", solve_a(&input()));
     println!("2023_01 b {}", solve_b(&input()));
@@ -7,25 +9,19 @@ fn main() {
 
 fn solve_a(input: &str) -> usize {
     let mut x = 0;
-    'lines: for (i, line) in input.lines().enumerate() {
-        let game_no = i + 1;
-        let rest = line
-            .strip_prefix(&format!("Game {}: ", game_no))
-            .expect("find prefix");
-        for draw in rest.split("; ") {
-            dbg!(&draw);
-            for ccount in draw.split(", ") {
-                let (n, color) = ccount.split_once(' ').unwrap();
-                let n: usize = n.parse().expect("parse n");
-                let color_ok = match color {
-                    "red" => n <= 12,
-                    "green" => n <= 13,
-                    "blue" => n <= 14,
-                    _ => panic!("{color:?} in {line:?}"),
-                };
-                if !color_ok {
-                    continue 'lines;
-                }
+    'game: for (game_no, game) in parse(input)
+        .into_iter()
+        .enumerate()
+        .map(|(i, x)| (i + 1, x))
+    {
+        for (n, color) in game {
+            if !match color {
+                "red" => n <= 12,
+                "green" => n <= 13,
+                "blue" => n <= 14,
+                _ => panic!("{color:?}"),
+            } {
+                continue 'game;
             }
         }
         x += game_no;
@@ -35,64 +31,58 @@ fn solve_a(input: &str) -> usize {
 
 fn solve_b(input: &str) -> usize {
     let mut x = 0;
-    for (i, line) in input.lines().enumerate() {
-        let game_no = i + 1;
-        let rest = line
-            .strip_prefix(&format!("Game {}: ", game_no))
-            .expect("find prefix");
+    for game in parse(input) {
         let mut game_min = [0, 0, 0];
-        for draw in rest.split("; ") {
-            dbg!(&draw);
-            for ccount in draw.split(", ") {
-                let (n, color) = ccount.split_once(' ').unwrap();
-                let n: usize = n.parse().expect("parse n");
-                let idx = match color {
-                    "red" => 0,
-                    "green" => 1,
-                    "blue" => 2,
-                    _ => panic!("{color:?} in {line:?}"),
-                };
-                game_min[idx] = std::cmp::max(game_min[idx], n);
-            }
+        for (n, color) in game {
+            let idx = match color {
+                "red" => 0,
+                "green" => 1,
+                "blue" => 2,
+                _ => panic!("{color:?}"),
+            };
+            game_min[idx] = std::cmp::max(game_min[idx], n);
         }
         x += game_min.iter().product::<usize>();
     }
     x
 }
 
+/// For each game (input line) return a vec of (count, color), regardless of rounds
+fn parse(input: &str) -> Vec<Vec<(usize, &str)>> {
+    let mut v = Vec::new();
+    let re = Regex::new(r"[;,] ").expect("Compile RE");
+    for (i, line) in input.lines().enumerate() {
+        let game_no = i + 1;
+        let mut game = Vec::new();
+        let rest = line
+            .strip_prefix(&format!("Game {}: ", game_no))
+            .expect("find prefix");
+        // The games don't actually matter; we only want to know each draw
+        for draw in re.split(rest) {
+            let (n, color) = draw.split_once(' ').unwrap();
+            let n: usize = n.parse().expect("parse n");
+            game.push((n, color));
+        }
+        v.push(game);
+    }
+    v
+}
+
 fn input() -> String {
     read_to_string("input/02.txt").unwrap()
 }
 
-struct Rgb {
-    red: usize,
-    green: usize,
-    blue: usize,
-}
-
-fn parse(s: &str) -> Vec<Vec<Rgb>> {
-    let mut games = Vec::new();
-    for (i, line) in s.lines().enumerate() {
-        let mut draws = Vec::new();
-        let rest = line
-            .strip_prefix(&format!("Game {}: ", i + 1))
-            .expect("find prefix");
-        games.push(draws);
-    }
-    games
-}
-
 #[cfg(test)]
 mod test {
-    use indoc::indoc;
-
     use super::*;
 
     #[test]
-    fn solution_a() {}
-
-    // b: 54623 is too high.
+    fn solution_a() {
+        assert_eq!(solve_a(&input()), 2369);
+    }
 
     #[test]
-    fn example_1() {}
+    fn solution_b() {
+        assert_eq!(solve_b(&input()), 66363);
+    }
 }
