bbadbdeca0e923871bd47005a550e3d2ea42a0e5
Martin Pool <mbp@sourcefrog.net>
2023-12-02 21:19:38 -0800

    Solve 2023_03

---
+++ 2023/src/bin/2023_03.rs
@@ -0,0 +1,139 @@
+use std::collections::{BTreeMap, BTreeSet};
+use std::fs::read_to_string;
+
+use aoclib::{point, Matrix, Point};
+
+fn main() {
+    let input = &input();
+    println!("{}", solve_b(input))
+}
+
+fn input() -> String {
+    read_to_string("2023/input/03.txt")
+        .or_else(|_| read_to_string("input/03.txt"))
+        .unwrap()
+}
+
+fn solve_a(input: &str) -> usize {
+    let mat = Matrix::from_string_lines(input);
+    let mut tot = 0;
+    for row in 0..mat.height() {
+        let mut num = String::new();
+        let mut near_sym = false;
+        for col in 0..mat.width() {
+            let p = point(col as isize, row as isize);
+            let c = mat[p];
+            if c.is_ascii_digit() {
+                num.push(c);
+                for (_np, nc) in mat.neighbors8(p) {
+                    if !nc.is_ascii_digit() && *nc != '.' {
+                        near_sym = true;
+                    }
+                }
+            } else if !num.is_empty() {
+                // end of this number
+                if near_sym {
+                    tot += num.parse::<usize>().expect("num is decimal");
+                }
+                num.clear();
+                near_sym = false;
+            }
+        }
+        if !num.is_empty() && near_sym {
+            tot += num.parse::<usize>().expect("num is decimal");
+        }
+    }
+    tot
+}
+
+fn solve_b(input: &str) -> usize {
+    let mat = Matrix::from_string_lines(input);
+    let mut gears: BTreeMap<Point, Vec<usize>> = BTreeMap::new();
+    for row in 0..mat.height() {
+        let mut num = String::new();
+        let mut near_gears: BTreeSet<Point> = Default::default();
+        for col in 0..mat.width() {
+            let p = point(col as isize, row as isize);
+            let c = mat[p];
+            if c.is_ascii_digit() {
+                num.push(c);
+                for (np, nc) in mat.neighbors8(p) {
+                    if *nc == '*' {
+                        near_gears.insert(np);
+                    }
+                }
+            } else if !num.is_empty() {
+                // end of this number
+                let val = num.parse::<usize>().expect("num is decimal");
+                for g in &near_gears {
+                    gears.entry(*g).or_default().push(val);
+                }
+                num.clear();
+                near_gears.clear();
+            }
+        }
+        if !num.is_empty() {
+            let val = num.parse::<usize>().expect("num is decimal");
+            for g in &near_gears {
+                gears.entry(*g).or_default().push(val);
+            }
+        }
+    }
+    let mut tot = 0;
+    for (_g, vs) in gears {
+        assert!(!vs.is_empty());
+        if vs.len() == 2 {
+            tot += vs.iter().product::<usize>();
+        }
+    }
+    tot
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let input = "\
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+";
+        assert_eq!(solve_a(input), 4361);
+    }
+
+    #[test]
+    fn example_2() {
+        let input = "\
+467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..
+";
+        assert_eq!(solve_b(input), 467835);
+    }
+
+    #[test]
+    fn solution_a() {
+        assert_eq!(solve_a(&input()), 556367);
+    }
+
+    #[test]
+    fn solution_b() {
+        assert_eq!(solve_b(&input()), 89471771);
+    }
+}
