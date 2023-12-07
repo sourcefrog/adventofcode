0e8a19b58506852d21d8f51d313341f8a3b14a94
Martin Pool <mbp@sourcefrog.net>
2023-12-06 19:11:41 -0800

    2023_06 a

---
+++ 2023/src/bin/2023_06.rs
@@ -0,0 +1,78 @@
+use std::fs::read_to_string;
+
+use itertools::Itertools;
+
+static YEAR: &str = "2023";
+static DAY: &str = "06";
+
+fn main() {
+    let input = &input();
+    println!("{YEAR}_{DAY} a {}", solve_a(input));
+    // println!("{YEAR}_{DAY} b {}", solve_b(input));
+}
+
+fn input() -> String {
+    read_to_string(format!("{YEAR}/input/{DAY}.txt"))
+        .or_else(|_| read_to_string(format!("input/{DAY}.txt")))
+        .unwrap()
+}
+
+/// Parse into a list of (time, distance) pairs, for each race.
+fn parse(input: &str) -> Vec<(usize, usize)> {
+    let (times, distances) = input
+        .lines()
+        .map(|l| {
+            l.split_ascii_whitespace()
+                .skip(1)
+                .map(|w| w.parse::<usize>().unwrap())
+                .collect_vec()
+        })
+        .collect_tuple()
+        .unwrap();
+    times.into_iter().zip_eq(distances).collect_vec()
+}
+
+fn solve_a(input: &str) -> usize {
+    let mut tot = 1;
+    for (time, distance) in parse(input) {
+        let mut wins = 0;
+        for speed in 1..time {
+            let travelled = (time - speed) * speed;
+            if travelled > distance {
+                wins += 1
+            }
+        }
+        tot *= wins;
+    }
+    tot
+}
+
+fn solve_b(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use indoc::indoc;
+
+    use super::*;
+
+    #[test]
+    fn example_1() {
+        let input = indoc! {"\
+            Time:      7  15   30
+            Distance:  9  40  200
+            "};
+        assert_eq!(solve_a(input), 288);
+    }
+
+    #[test]
+    fn solution_a() {
+        assert_eq!(solve_a(&input()), 252_000);
+    }
+
+    #[test]
+    fn solution_b() {
+        // assert_eq!(solve_b(&input()), 13114317);
+    }
+}
