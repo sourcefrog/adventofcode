a0356ca09946820a1dfa7f2bc3762eb3373cb918
Martin Pool <mbp@sourcefrog.net>
2023-12-06 20:57:41 -0800

    Start range overlap tests

--- aoclib/src/range.rs
+++ aoclib/src/range.rs
@@ -1,15 +1,30 @@
+use std::cmp::{max, min};
 use std::ops::Range;
 
 pub trait RangeExt: Sized {
     fn overlap(&self, other: &Self) -> [Self; 3];
 }
 
-impl<R> RangeExt for Range<R> {
+impl<R: Ord + Copy> RangeExt for Range<R> {
     /// Split self into the range that comes before `other`, that is contained within `other`,
     /// and that comes after `other`.
     ///
     /// Any of the resulting ranges might be empty: use [Range::is_empty].
     fn overlap(&self, other: &Self) -> [Self; 3] {
-        todo!()
+        [
+            self.start..min(self.end, other.start),
+            max(self.start, other.start)..min(self.end, other.end),
+            max(self.start, other.end)..max(self.end, other.end),
+        ]
+    }
+}
+
+#[cfg(test)]
+mod test_super {
+    use super::*;
+
+    #[test]
+    fn overlap() {
+        assert_eq!((0..10).overlap(&(10..20)), [0..10, 10..10, 20..20])
     }
 }
