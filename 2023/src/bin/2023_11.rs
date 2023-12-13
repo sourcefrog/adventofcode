b1e1e911c096a9f09a87f779aed79fe60db76339
Martin Pool <mbp@sourcefrog.net>
2023-12-13 08:20:15 -0800

    2023_11 b

--- 2023/src/bin/2023_11.rs
+++ 2023/src/bin/2023_11.rs
@@ -1,4 +1,8 @@
-use std::collections::HashSet;
+//! Cosmic Expansion
+//!
+//! <https://adventofcode.com/2023/day/11>
+
+use std::cmp::max;
 use std::fs::read_to_string;
 
 use aoclib::Matrix;
@@ -19,37 +23,48 @@ fn input() -> String {
         .unwrap()
 }
 
-fn solve_a(input: &str) -> usize {
-    let map = Matrix::from_string_lines(input);
-    let stars = map.find_values(&'#').collect_vec();
-    // dbg!(&stars);
-    let star_xs: HashSet<isize> = stars.iter().map(|p| p.x).unique().collect();
-    let star_ys: HashSet<isize> = stars.iter().map(|p| p.y).unique().collect();
-    /* Between each distinct pair (regardless of order), count rows and columns, and count
-     * twice any row or column with no stars. */
-    /* Instead of iterating X and Y values, it would probably be faster to compute
-     * once the expanded X and Y values, adjusting the star positions, and then we
-     * can take the Manhattan distance directly. */
-    let mut total = 0;
-    for s1 in stars.iter() {
-        for s2 in stars.iter() {
-            let mut dist = 0;
-            /* Count only to the right and down, which will have the effect of
-             * counting each pair once. */
-            for x in s1.x..s2.x {
-                dist += 1 + (!star_xs.contains(&x)) as usize;
-            }
-            for y in s1.y..s2.y {
-                dist += 1 + (!star_ys.contains(&y)) as usize;
-            }
-            total += dist;
-        }
-    }
-    total
+fn solve_a(input: &str) -> isize {
+    solve(input, 2)
 }
 
-fn solve_b(input: &str) -> usize {
-    0
+fn solve_b(input: &str) -> isize {
+    solve(input, 1_000_000)
+}
+
+/// Solve the general case, with each empty row/column made larger by a given factor.
+fn solve(input: &str, expansion: isize) -> isize {
+    /* All we really need is the set of x and y values to calculate the Manhattan distances: the point identities don't actually matter.  */
+    let stars = Matrix::from_string_lines(input)
+        .find_values(&'#')
+        .collect_vec();
+    let expanded_xs = expand(stars.iter().map(|p| p.x), expansion);
+    let expanded_ys = expand(stars.iter().map(|p| p.y), expansion);
+    expanded_xs
+        .iter()
+        .tuple_combinations()
+        .map(|(a, b)| max(b - a, 0))
+        .sum::<isize>()
+        + expanded_ys
+            .iter()
+            .tuple_combinations()
+            .map(|(a, b)| max(b - a, 0))
+            .sum::<isize>()
+}
+
+fn expand(xs: impl IntoIterator<Item = isize>, expansion: isize) -> Vec<isize> {
+    /* Increase all the xs to allow for blank columns. There may be duplicates. */
+    assert!(expansion >= 1);
+    let mut last_x = None;
+    let mut offset = 0;
+    let mut expanded_xs = Vec::new();
+    for x in xs.into_iter().sorted() {
+        if let Some(last_x) = last_x {
+            offset += (expansion - 1) * max(x - last_x - 1, 0);
+        }
+        expanded_xs.push(x + offset);
+        last_x = Some(x);
+    }
+    expanded_xs
 }
 
 #[cfg(test)]
@@ -77,12 +92,18 @@ mod test {
     }
 
     #[test]
+    fn example_2() {
+        assert_eq!(solve(EXAMPLE, 10), 1030);
+        assert_eq!(solve(EXAMPLE, 100), 8410);
+    }
+
+    #[test]
     fn solution_a() {
         assert_eq!(solve_a(&input()), 9403026)
     }
 
     #[test]
     fn solution_b() {
-        // assert_eq!(solve_b(&input()), 13114317);
+        assert_eq!(solve_b(&input()), 543018317006);
     }
 }
