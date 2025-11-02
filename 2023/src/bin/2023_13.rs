46f1d11cdb5b3f2b533a9338cc125e1f8b7be9ce
Martin Pool <mbp@sourcefrog.net>
2025-11-02 09:37:21 -0800

    Solve 2023_13

--- 2023/src/bin/2023_13.rs
+++ 2023/src/bin/2023_13.rs
@@ -6,60 +6,110 @@ static PUZZLE: &str = env!("CARGO_BIN_NAME");
 
 fn main() {
     let input = &input();
-    println!("{PUZZLE} a {}", solve_a(input));
-    println!("{PUZZLE} b {}", solve_b(input));
+    println!("{PUZZLE} a {}", solve_one(input));
+    println!("{PUZZLE} b {}", solve_two(input));
 }
 
 fn input() -> String {
-    let path = format!("../input/{PUZZLE}.txt");
-    read_to_string(path).expect("Failed to read input")
+    let path = &format!("../input/{PUZZLE}.txt");
+    read_to_string(path)
+        .or_else(|_| read_to_string("..".to_owned() + path))
+        .expect("Failed to read input")
 }
 
-fn solve_a(input: &str) -> usize {
-    let mut tot = 0;
+fn split_inputs(input: &str) -> Vec<Matrix<bool>> {
+    let mut matrixes = Vec::new();
     for block in input.split("\n\n") {
         let map = Matrix::from_string_lines(block).map(|c| *c == '#');
-        dbg!(map.width(), map.height());
-        tot += find_reflections(&map);
+        matrixes.push(map)
     }
-    tot
+    matrixes
 }
 
-fn find_reflections(map: &Matrix<bool>) -> usize {
+#[derive(Debug, Copy, Clone, Eq, PartialEq)]
+enum RowOrCol {
+    Row(usize),
+    Col(usize),
+}
+
+impl RowOrCol {
+    fn summary(&self) -> usize {
+        match self {
+            RowOrCol::Row(x) => 100 * x,
+            RowOrCol::Col(x) => *x,
+        }
+    }
+}
+
+fn solve_one(input: &str) -> usize {
+    split_inputs(input)
+        .into_iter()
+        .map(|map| find_one_reflection(&map).summary())
+        .sum()
+}
+
+fn find_one_reflection(map: &Matrix<bool>) -> RowOrCol {
+    let refls = find_reflections(map);
+    assert_eq!(refls.len(), 1);
+    refls[0]
+}
+
+fn find_reflections(map: &Matrix<bool>) -> Vec<RowOrCol> {
     let h = map.height();
     let w = map.width();
+    let mut res = Vec::new();
 
-    println!("{}", map.to_string_lines());
+    // println!("{}", map.to_string_lines());
 
     'col: for xmirror in 1..w {
+        // TODO: Choose a range for x1 such that x2 will be valid, and similarly for y.
         for x1 in 0..xmirror {
             let x2 = (xmirror - x1) + xmirror - 1;
-            println!("{xmirror}: check col {x1} against {x2}");
+            // println!("{xmirror}: check col {x1} against {x2}");
             if x2 < w && !map.column(x1).eq(map.column(x2)) {
-                println!("   >mismatch");
+                // println!("   >mismatch");
                 continue 'col;
             }
         }
-        println!("found reflection at x={xmirror}");
-        return xmirror;
+        // println!("found reflection at x={xmirror}");
+        res.push(RowOrCol::Col(xmirror));
     }
 
     'row: for ymirror in 1..h {
         for y in 0..=ymirror {
             let y2 = (ymirror - y) + (ymirror - 1);
-            println!("{ymirror}: check {y} against {y2}");
+            // println!("{ymirror}: check {y} against {y2}");
             if y2 < h && !map.row(y).eq(map.row(y2)) {
                 continue 'row;
             }
         }
-        println!("found reflection at y={ymirror}");
-        return 100 * ymirror;
+        // println!("found reflection at y={ymirror}");
+        res.push(RowOrCol::Row(ymirror));
     }
-    unreachable!("No reflection found in\n{}", map.to_string_lines());
+    res
 }
 
-fn solve_b(_input: &str) -> usize {
-    todo!();
+fn solve_two(input: &str) -> usize {
+    let mut sum = 0;
+    'map: for map in split_inputs(input) {
+        let orig = find_one_reflection(&map);
+
+        // Try inverting all points until we find one that generates a different reflection
+        for (smudge_point, val) in map.point_values() {
+            let mut altered = map.clone();
+            altered[smudge_point] = !val;
+
+            let mut altered_refl = find_reflections(&altered);
+            altered_refl.retain(|rc| *rc != orig);
+            if altered_refl.len() == 1 {
+                // println!("found smudge point {smudge_point:?}");
+                sum += altered_refl[0].summary();
+                continue 'map;
+            }
+        }
+        unreachable!("didn't find a smudge point");
+    }
+    sum
 }
 
 #[cfg(test)]
@@ -88,16 +138,21 @@ mod test {
 
     #[test]
     fn example_1() {
-        assert_eq!(solve_a(EXAMPLE), 405);
+        assert_eq!(solve_one(EXAMPLE), 405);
     }
 
     #[test]
-    fn solution_a() {
-        assert_eq!(solve_a(&input()), 27505);
+    fn solution_one() {
+        assert_eq!(solve_one(&input()), 27505);
     }
 
     #[test]
-    fn solution_b() {
-        // assert_eq!(solve_b(&input()), 13114317);
+    fn example_two() {
+        assert_eq!(solve_two(EXAMPLE), 400);
+    }
+
+    #[test]
+    fn solution_two() {
+        assert_eq!(solve_two(&input()), 22906);
     }
 }
