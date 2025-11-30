56552a57f3ba16def9aeea0d3dd74c43d71e249d
Martin Pool <mbp@sourcefrog.net>
2025-11-30 09:10:25 -0800

    Solve 2023_15 part 2

--- 2023/src/bin/2023_15.rs
+++ 2023/src/bin/2023_15.rs
@@ -23,7 +23,37 @@ fn solve_one(input: &str) -> usize {
 }
 
 fn solve_two(input: &str) -> usize {
-    0
+    let mut boxes: Vec<Vec<(&str, usize)>> = vec![Vec::new(); 256];
+    for step in input.trim().split(',') {
+        if let Some(label) = step.strip_suffix('-') {
+            boxes[hash(label)].retain(|(l, _fl)| *l != label);
+        } else {
+            let len = step.len();
+            let label = &step[..len - 2];
+            assert_eq!(step.as_bytes()[len - 2], b'=');
+            let fl = (step.as_bytes()[len - 1] as char)
+                .to_digit(10)
+                .expect("not a digit") as usize;
+            if let Some(x) = boxes[hash(label)].iter_mut().find(|(l, _)| *l == label) {
+                x.1 = fl;
+            } else {
+                boxes[hash(label)].push((label, fl))
+            }
+        }
+    }
+    focus_power(&boxes)
+}
+
+fn focus_power(boxes: &Vec<Vec<(&str, usize)>>) -> usize {
+    boxes
+        .iter()
+        .enumerate()
+        .flat_map(|(ibox, abox)| {
+            abox.iter()
+                .enumerate()
+                .map(move |(ilens, (_label, fl))| (1 + ibox) * (1 + ilens) * fl)
+        })
+        .sum()
 }
 
 #[cfg(test)]
@@ -45,7 +75,20 @@ mod test {
     }
 
     #[test]
+    fn example_3() {
+        assert_eq!(
+            solve_two("rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"),
+            145
+        );
+    }
+
+    #[test]
     fn solution_1() {
         assert_eq!(solve_one(&input()), 517551);
+    }
+
+    #[test]
+    fn solution_2() {
+        assert_eq!(solve_two(&input()), 286097);
     }
 }
