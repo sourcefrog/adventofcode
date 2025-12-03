39fd832e7e0843dcb8a4240dae456a999b0372b7
Martin Pool <mbp@sourcefrog.net>
2025-12-03 08:18:26 -0800

    2025_01 part 2: slightly brutal solution

--- 2025/src/bin/2025_01.rs
+++ 2025/src/bin/2025_01.rs
@@ -1,3 +1,5 @@
+const SIZE: isize = 100;
+
 fn main() {
     let input = input();
     println!("{}", solve1(&input));
@@ -9,7 +11,6 @@ fn input() -> String {
 }
 
 fn solve1(input: &str) -> usize {
-    const SIZE: isize = 100;
     let mut p: isize = 50;
     let mut zeros = 0;
     for l in input.trim().lines() {
@@ -33,24 +34,18 @@ fn solve1(input: &str) -> usize {
 }
 
 fn solve2(input: &str) -> usize {
-    const SIZE: isize = 100;
     let mut p: isize = 50;
-    let mut clicks: usize = 0;
+    let mut clicks: isize = 0;
     for l in input.trim().lines().map(str::trim) {
         let dist: isize = l[1..].parse().unwrap();
         match l.chars().next().unwrap() {
             'R' => {
-                clicks += usize::try_from((p + dist) / SIZE).unwrap();
+                clicks += (p + dist) / SIZE;
                 p = (p + dist) % SIZE;
             }
             'L' => {
                 let q = p - dist;
-                if q <= 0 {
-                    clicks += usize::try_from(-q / SIZE).unwrap();
-                    if q % SIZE == 0 {
-                        clicks += 1
-                    }
-                }
+                clicks += clicks_left(p, dist);
                 p = q % SIZE;
                 if p < 0 {
                     p += SIZE
@@ -62,6 +57,22 @@ fn solve2(input: &str) -> usize {
         println!("{l}\t{clicks}\t{p}");
     }
     // 6499 is too high
+    clicks.try_into().unwrap()
+}
+
+/// If you start at position `p` and move left by `dist` how many times do
+/// we pass 0, including ending on zero?
+fn clicks_left(p: isize, dist: isize) -> isize {
+    assert!((0..SIZE).contains(&p));
+    let mut q = p - dist;
+    let mut clicks = 0;
+    if p == 0 {
+        clicks -= 1;
+    }
+    while q <= 0 {
+        clicks += 1;
+        q += SIZE;
+    }
     clicks
 }
 
@@ -82,8 +93,24 @@ mod test {
     ";
 
     #[test]
+    fn left_examples() {
+        assert_eq!(clicks_left(50, 68), 1);
+        assert_eq!(clicks_left(82, 30), 0);
+        assert_eq!(clicks_left(0, 5), 0);
+        assert_eq!(clicks_left(55, 55), 1);
+        assert_eq!(clicks_left(0, 1), 0);
+        assert_eq!(clicks_left(99, 99), 1);
+        assert_eq!(clicks_left(14, 82), 1);
+    }
+
+    #[test]
     fn solution1() {
         assert_eq!(solve1(&input()), 997);
+    }
+
+    #[test]
+    fn solution2() {
+        assert_eq!(solve2(&input()), 5978);
     }
 
     #[test]
