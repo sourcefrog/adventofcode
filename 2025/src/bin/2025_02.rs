4cf93410c2158be50f526cd4951deef63c20ff79
Martin Pool <mbp@sourcefrog.net>
2025-12-22 20:39:55 -0800

    2025_02

--- 2025/src/bin/2025_02.rs
+++ 2025/src/bin/2025_02.rs
@@ -19,24 +19,86 @@ fn parse(input: &str) -> Vec<(usize, usize)> {
         .collect()
 }
 
-fn is_repeated_decimal(x: usize) -> bool {
-    // a bit of a cheesy way to work out if the digits repeat
-    let s = x.to_string();
-    let l = s.len();
-    let b = s.as_bytes();
-    b[0..(l / 2)] == b[(l / 2)..]
+fn decimal_len(x: usize) -> usize {
+    x.ilog10() as usize + 1
+}
+
+/// True if the decimal form of `x` contains `repeats` copies of its first digits.
+fn is_repeated_decimal(x: usize, repeats: usize) -> bool {
+    assert!(repeats >= 2);
+    if x == 0 {
+        return false;
+    }
+    let digits = decimal_len(x);
+    if !digits.is_multiple_of(repeats) {
+        return false;
+    }
+    let s = 10usize.pow((digits / repeats) as u32);
+    let v = x % s;
+    x == repeat(v, repeats)
+}
+
+/// True if repeated with any length
+fn is_repeat(x: usize) -> bool {
+    assert!(x > 0);
+    let digits = decimal_len(x);
+    let mut s = 1;
+    for d in 1..=(digits / 2) {
+        s *= 10;
+        if !digits.is_multiple_of(d) {
+            continue;
+        }
+        let r = digits / d;
+        let v = x % s;
+        let y = (0..r).map(|j| s.pow(j as u32) * v).sum();
+        if x == y {
+            return true;
+        }
+    }
+    false
+}
+
+/// Return the number that has `r` repeats of the digits in x.
+fn repeat(x: usize, r: usize) -> usize {
+    debug_assert!(r >= 2);
+    if x == 0 {
+        return 0;
+    }
+    let d = decimal_len(x);
+    let s = 10usize.pow(d as u32);
+    let mut a = x;
+    for i in 1..r {
+        a += s.pow(i as u32) * x;
+    }
+    a
 }
 
 fn solve1(input: &str) -> usize {
     parse(input)
         .into_iter()
         .flat_map(|(a, b)| a..=b)
-        .filter(|x| is_repeated_decimal(*x))
+        .filter(|x| is_repeated_decimal(*x, 2))
         .sum()
 }
 
-fn solve2(_input: &str) -> usize {
-    0
+fn solve2(input: &str) -> usize {
+    let input = parse(input);
+    let mut s = 0;
+    for (a, b) in input {
+        // We don't really need to check all of them: if we're at
+        // 12350000 then we really ought to be able to see that the next
+        // useful number to check is 12351235 (at least for 2 repeats).
+        // Although, it might get a bit complicated because we need
+        // to be careful not to double count e.g. 333333 as being a repeat
+        // of 3, 33 and 333 ...
+        'i: for i in a..=b {
+            if is_repeat(i) {
+                s += i;
+                continue 'i;
+            }
+        }
+    }
+    s
 }
 
 #[cfg(test)]
@@ -53,12 +115,23 @@ mod test {
     }
 
     #[test]
+    fn example2() {
+        assert_eq!(solve2(EXAMPLE), 4174379265);
+    }
+
+    #[test]
     fn solution1() {
         assert_eq!(solve1(&input()), 53420042388);
     }
 
     #[test]
     fn solution2() {
-        // assert_eq!(solve2(&input()), 5978);
+        assert_eq!(solve2(&input()), 69553832684);
+    }
+
+    #[test]
+    fn test_is_repeat() {
+        assert!(is_repeat(11));
+        assert!(is_repeat(1010));
     }
 }
