92f0b5e35a90b0f16977dd994d1f58ee5e9b09c9
Martin Pool <mbp@sourcefrog.net>
2025-12-23 21:01:44 -0800

    Add a template

---
+++ 2025/src/bin/2025_template.rs
@@ -0,0 +1,48 @@
+use itertools::Itertools;
+
+fn main() {
+    let input = input();
+    println!("{}", solve1(&input));
+    println!("{}", solve2(&input));
+}
+
+fn input() -> String {
+    aoclib::input!()
+}
+
+fn solve1(input: &str) -> usize {
+    0
+}
+
+fn solve2(input: &str) -> usize {
+    0
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    static EXAMPLE: &str = indoc::indoc! {
+                                                "\
+    "};
+
+    #[test]
+    fn example1() {
+        assert_eq!(solve1(EXAMPLE), 0);
+    }
+
+    #[test]
+    fn example2() {
+        assert_eq!(solve2(EXAMPLE), 0);
+    }
+
+    // #[test]
+    // fn solution1() {
+    //     assert_eq!(solve1(&input()), 0);
+    // }
+
+    // #[test]
+    // fn solution2() {
+    //     assert_eq!(solve2(&input()), 0);
+    // }
+}
